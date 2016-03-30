bdf83fa060617ef891e8db4ed771e16d7667a767
yangfuhai <fuhai999@163.com>
2016-03-30 19:26:52 +0800

    init continue...

--- jpress/src/io/jpress/controller/admin/_AdminController.java
+++ jpress/src/io/jpress/controller/admin/_AdminController.java
@@ -30,53 +30,46 @@ import io.jpress.utils.EncryptCookieUtils;
 import io.jpress.utils.HashUtils;
 import io.jpress.utils.StringUtils;
 
-@UrlMapping(url="/admin" ,viewPath ="/WEB-INF/admin")
+@UrlMapping(url = "/admin", viewPath = "/WEB-INF/admin")
 @Before(AdminInterceptor.class)
 public class _AdminController extends JBaseController {
-	
-	public void index(){
+
+	public void index() {
 		render("index.html");
 	}
-	
-	
+
 	@Clear
-	public void login(){
+	public void login() {
 		String username = getPara("username");
 		String password = getPara("password");
-		
-		if(!StringUtils.areNotEmpty(username,password)){
+
+		if (!StringUtils.areNotEmpty(username, password)) {
 			render("login.html");
 			return;
 		}
-		
+
 		User user = User.findUserByUsername(username);
-		
-		if(null == user){
+
+		if (null == user) {
 			renderAjaxResultForError("没有该用户");
 			return;
 		}
-		
-		if(HashUtils.verlifyUser(user, password) && user.isAdministrator()){
-			
+
+		if (HashUtils.verlifyUser(user, password) && user.isAdministrator()) {
+
 			MessageKit.sendMessage(Actions.USER_LOGINED, user);
-			
+
 			EncryptCookieUtils.put(this, Consts.COOKIE_LOGIN_USER_ID, user.getId());
 			CacheKit.put("user", user.getId(), user);
 			renderAjaxResultForSuccess("登陆成功");
-		}else{
+		} else {
 			renderAjaxResultForError("密码错误");
 		}
 	}
-	
-	public void logout(){
+
+	public void logout() {
 		EncryptCookieUtils.remove(this, Consts.COOKIE_LOGIN_USER_ID);
 		redirect("/admin");
 	}
-	
-	
-	public void blank(){
-		render("blank.html");
-	}
-	
 
 }

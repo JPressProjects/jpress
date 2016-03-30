81a86bb0a62001bd043631f27630121b210860ea
yangfuhai <fuhai999@163.com>
2016-03-30 10:01:11 +0800

    enhance EncryptCookieUtils

--- jpress/src/io/jpress/utils/EncryptCookieUtils.java
+++ jpress/src/io/jpress/utils/EncryptCookieUtils.java
@@ -39,10 +39,21 @@ public class EncryptCookieUtils {
 	}
 	
 	public static void put(Controller ctr, String key, String value,int maxAgeInSeconds) {
-		String cookie_encrypt_key = PropKit.get("encrypt_key");
-		String cookieValue = value + COOKIE_SEPARATOR + HashKit.md5(cookie_encrypt_key + value);
+		String encrypt_key = PropKit.get("encrypt_key");
+		String saveTime = System.currentTimeMillis()+"";
+		String encrypt_value = encrypt(encrypt_key, saveTime,maxAgeInSeconds+"", value);
+		
+		String cookieValue = encrypt_value+COOKIE_SEPARATOR
+							+saveTime+COOKIE_SEPARATOR
+							+maxAgeInSeconds+COOKIE_SEPARATOR
+							+value;
+		
 		ctr.setCookie(key, cookieValue, maxAgeInSeconds);
 		
+	}
+
+	private static String encrypt(String encrypt_key, String saveTime, String maxAgeInSeconds,String value ) {
+		return HashKit.md5(encrypt_key+saveTime+maxAgeInSeconds+value);
 	}
 
 	public static void remove(Controller ctr, String key) {
@@ -51,20 +62,51 @@ public class EncryptCookieUtils {
 
 	public static String get(Controller ctr, String key) {
 		
-		String cookie_encrypt_key = PropKit.get("encrypt_key");
+		String encrypt_key = PropKit.get("encrypt_key");
 		String cookieValue = ctr.getCookie(key);
 		
-		if (null != cookieValue) {
-			String userinfos[] = cookieValue.split(COOKIE_SEPARATOR);
-			if (null != userinfos && 2 == userinfos.length) {
-				// 保证 cookie 不被人为修改
-				if (userinfos[1].equals(HashKit.md5(cookie_encrypt_key + userinfos[0]))) {
-					return userinfos[0];
+		if (StringUtils.isNotBlank(cookieValue)) {
+			String cookieStrings[] = cookieValue.split(COOKIE_SEPARATOR);
+			if (null != cookieStrings && 4 == cookieStrings.length) {
+				String encrypt_value = cookieStrings[0];
+				String saveTime = cookieStrings[1];
+				String maxAgeInSeconds = cookieStrings[2];
+				String value = cookieStrings[3];
+				
+				String encrypt = encrypt(encrypt_key, saveTime, maxAgeInSeconds, value);
+				
+				//保证 cookie 不被人为修改
+				if(encrypt_value!=null && encrypt_value.equals(encrypt)){
+					long stime = Long.parseLong(saveTime);
+					long maxtime = Long.parseLong(maxAgeInSeconds) * 1000;
+					
+					//查看是否过时
+					if(System.currentTimeMillis() - (stime + maxtime) > 0){
+						return value;
+					}
 				}
 			}
 		}
-
 		return null;
+	}
+	
+	public static Long getLong(Controller ctr,String key){
+		String value = get(ctr, key);
+		return null == value ? null : Long.parseLong(value);
+	}
+	
+	public static long getLong(Controller ctr,String key,long defalut){
+		String value = get(ctr, key);
+		return null == value ? defalut : Long.parseLong(value);
+	}
+	public static Integer getInt(Controller ctr,String key){
+		String value = get(ctr, key);
+		return null == value ? null : Integer.parseInt(value);
+	}
+	
+	public static int getLong(Controller ctr,String key,int defalut){
+		String value = get(ctr, key);
+		return null == value ? defalut : Integer.parseInt(value);
 	}
 
 }

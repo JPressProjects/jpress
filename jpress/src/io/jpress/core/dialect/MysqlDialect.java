298f07ba97095710964b55132e3ab2ed4b882d46
yangfuhai <fuhai999@163.com>
2016-03-31 16:54:15 +0800

    init continue...

--- jpress/src/io/jpress/core/dialect/MysqlDialect.java
+++ jpress/src/io/jpress/core/dialect/MysqlDialect.java
@@ -91,9 +91,9 @@ public class MysqlDialect extends DbDialect {
 	
 	public String doTableConvert(String sql) {
 		for (Map.Entry<String, String> entry : tableMapping.entrySet()) {
-			sql = sql.replace(entry.getKey() + " ", String.format("`%s` ", entry.getValue()));
-			sql = sql.replace(entry.getKey() + ",", String.format("`%s`,", entry.getValue()));
-			sql = sql.replace(entry.getKey() + ".", String.format("`%s`.", entry.getValue()));
+			sql = sql.replace(" "+entry.getKey() + " ", String.format(" `%s` ", entry.getValue()));
+			sql = sql.replace(" "+entry.getKey() + ",", String.format(" `%s`,", entry.getValue()));
+			sql = sql.replace(" "+entry.getKey() + ".", String.format(" `%s`.", entry.getValue()));
 //			sql = sql.replace(entry.getKey() + "`", entry.getValue() + "`");
 		}
 		return sql;

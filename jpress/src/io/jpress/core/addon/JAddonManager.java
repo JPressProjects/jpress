d2459eaf7dec16303db42bb6aa7c0c04cb70781d
yangfuhai <fuhai999@163.com>
2016-04-14 13:06:02 +0800

    for v0.0.1

---
+++ jpress/src/io/jpress/core/addon/JAddonManager.java
@@ -0,0 +1,14 @@
+package io.jpress.core.addon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JAddonManager {
+
+	List<JAddon> addonList = new ArrayList<JAddon>();
+
+	public void loadAddon() {
+
+	}
+
+}
---
+++ jpress/src/io/jpress/core/addon/JaddonPlugin.java
@@ -0,0 +1,17 @@
+package io.jpress.core.addon;
+
+import com.jfinal.plugin.IPlugin;
+
+public class JaddonPlugin implements IPlugin {
+
+	@Override
+	public boolean start() {
+		return false;
+	}
+
+	@Override
+	public boolean stop() {
+		return false;
+	}
+
+}

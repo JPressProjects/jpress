00015fe344bed9788aff8a2170187bd1617ccfc8
yangfuhai <fuhai999@163.com>
2016-04-21 21:55:56 +0800

    for v0.0.1

--- jpress/src/io/jpress/model/Comment.java
+++ jpress/src/io/jpress/model/Comment.java
@@ -29,13 +29,21 @@ public class Comment extends BaseComment<Comment> {
 	public Page<Comment> doPaginate(int pageNumber, int pageSize, String module, String type) {
 
 		String select = " select c.*,content.title content_title,u.username";
+		String sqlExceptSelect = " from comment c " + "left join content on c.content_id = content.id "
+				+ "left join `user` u on c.user_id = u.id " + "order by c.created";
+
+		return paginate(pageNumber, pageSize, select, sqlExceptSelect);
+	}
+
+	public Page<Comment> doPaginateByContentId(int pageNumber, int pageSize, long contentId) {
+
+		String select = " select c.*,content.title content_title,u.username";
 		String sqlExceptSelect = " from comment c " 
 				+ "left join content on c.content_id = content.id "
 				+ "left join `user` u on c.user_id = u.id " 
-				+ "order by c.created";
+				+ "where c.content_id = ? " + "order by c.created";
 
-		return paginate(pageNumber, pageSize, select, sqlExceptSelect);
-
+		return paginate(pageNumber, pageSize, select, sqlExceptSelect, contentId);
 	}
 
 	public String getUsername() {

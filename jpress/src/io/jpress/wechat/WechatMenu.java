c72aa439fb7ff8f2d18025295a13f8766cc5e491
yangfuhai <fuhai999@163.com>
2016-04-28 11:45:22 +0800

    for v 0.0.1

--- jpress/src/io/jpress/wechat/WechatMenu.java
+++ jpress/src/io/jpress/wechat/WechatMenu.java
@@ -1,5 +1,27 @@
 package io.jpress.wechat;
 
+import com.alibaba.fastjson.JSONObject;
+
 public class WechatMenu {
+	
+	
+	
+	
+	
+
+	
+	
+	public static JSONObject toJSONObject(WechatMenuItem item){
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("name", item.getName());
+		jsonObject.put("type", item.getType());
+		
+		if("view".equals(item.getType())){
+			jsonObject.put("url", item.getKey());
+		}else{
+			jsonObject.put("key", item.getKey());
+		}
+		return jsonObject;
+	}
 
 }
--- jpress/src/io/jpress/wechat/WechatMenuItem.java
+++ jpress/src/io/jpress/wechat/WechatMenuItem.java
@@ -1,5 +1,54 @@
 package io.jpress.wechat;
 
+import java.math.BigInteger;
+
+import com.alibaba.fastjson.JSONObject;
+
 public class WechatMenuItem {
+
+	public static final String TYPE_CLICK = "click"; //点击推事件
+	public static final String TYPE_VIEW = "view"; //跳转URL
+	public static final String TYPE_SCANCODE_PUSH = "scancode_push"; //扫码推事件
+	public static final String TYPE_SCANCODE_WAITMSG = "scancode_waitmsg"; //扫码推事件且弹出“消息接收中”提示框
+	public static final String TYPE_PIC_SYSPHOTO = "pic_sysphoto"; //弹出系统拍照发图
+	public static final String TYPE_PIC_PHOTO_OR_ALBUM = "pic_photo_or_album"; //弹出拍照或者相册发图
+	public static final String TYPE_PIC_WEIXIN = "pic_weixin"; //弹出微信相册发图器
+	public static final String TYPE_LOCATION_SELECT = "location_select"; //弹出地理位置选择器
+	
+	//以下 TYPE_MEDIA_ID 和 TYPE_VIEW_LIMITED 是专门给第三方平台旗下未微信认证
+	public static final String TYPE_MEDIA_ID = "media_id"; //下发消息（除文本消息）
+	public static final String TYPE_VIEW_LIMITED = "view_limited"; //跳转图文消息URL
+
+	private String name;
+	private String type;
+	private String key;
+	
+	private BigInteger id;
+	private BigInteger pid;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
 
 }

cd603c8899b70c4f335a69620b8d6f7f4e42244d
yangfuhai <fuhai999@163.com>
2016-05-23 23:06:29 +0800

    for v 0.0.1

--- AddonHelloWorld/src/io/jpress/addon/helloworld/HelloHook.java
+++ AddonHelloWorld/src/io/jpress/addon/helloworld/HelloHook.java
@@ -18,12 +18,13 @@ package io.jpress.addon.helloworld;
 import com.jfinal.render.Render;
 import com.jfinal.render.TextRender;
 
+import io.jpress.core.addon.AddonController;
 import io.jpress.core.addon.Hook;
-import io.jpress.core.addon.HookController;
 
 public class HelloHook extends Hook {
 
-	public Render process_controller(HookController controller) {
+	@Override
+	public Render process_controller(AddonController controller) {
 		return new TextRender("hello addon");
 	}
 

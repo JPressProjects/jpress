85ae5d9197b0d975c351baf3b0bb8123cafe1b2f
yangfuhai <fuhai999@163.com>
2016-05-25 15:08:07 +0800

    for v 0.0.1

--- jpress/WebRoot/static/jpress/admin/js/common.js
+++ jpress/WebRoot/static/jpress/admin/js/common.js
@@ -1,8 +1,12 @@
 $(function() {
-
+	$(".jp-onmouse").mouseover(function() {
+		$(this).find(".row-actions").show();
+	}).mouseout(function() {
+		$(".row-actions").hide()
+	})
 });
 
-function check_all(checkbox) {
+function checkAll(checkbox) {
 	var items = document.getElementsByName("dataItem");
 	for (var i = 0; i < items.length; i++) {
 		items[i].checked = checkbox.checked;

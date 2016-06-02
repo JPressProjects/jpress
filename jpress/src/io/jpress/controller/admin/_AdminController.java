fae557cfd4c7fb88fe9784549c0d0194e210717b
yangfuhai <fuhai999@163.com>
2016-06-02 12:05:18 +0800

    for v 0.0.1

--- jpress/src/io/jpress/controller/admin/_AdminController.java
+++ jpress/src/io/jpress/controller/admin/_AdminController.java
@@ -57,7 +57,7 @@ public class _AdminController extends JBaseController {
 			setAttr("contents", contents);
 		}
 
-		Page<Comment> commentPage = Comment.DAO.doPaginate(1, 10, null, null, null, null);
+		Page<Comment> commentPage = Comment.DAO.doPaginateWithContent(1, 10, null, null, null, null);
 		if (commentPage != null) {
 			setAttr("comments", commentPage.getList());
 		}

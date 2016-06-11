54fe682d956832cc633998aaeb08cbf7efe007b5
yangfuhai <fuhai999@163.com>
2016-06-11 12:32:42 +0800

    for v 0.0.1

--- jpress/src/io/jpress/oauth2/connector/GithubConnector.java
+++ jpress/src/io/jpress/oauth2/connector/GithubConnector.java
@@ -15,19 +15,52 @@
  */
 package io.jpress.oauth2.connector;
 
+import com.alibaba.fastjson.JSONObject;
+
 import io.jpress.oauth2.OauthConnector;
 import io.jpress.oauth2.OauthUser;
 
 public class GithubConnector extends OauthConnector {
 	@Override
 	public String createAuthorizeUrl(String state) {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sb = new StringBuilder("https://github.com/login/oauth/authorize?");
+		sb.append("scope=user");
+		sb.append("&client_id=" + getClientId());
+		sb.append("&redirect_uri=" + getRedirectUri());
+		sb.append("&state=" + state);
+
+		return sb.toString();
 	}
 
 	@Override
 	protected OauthUser getOauthUser(String code) {
-		// TODO Auto-generated method stub
+		String accessToken = getAccessToken(code);
+
+		String url = "https://api.github.com/user?access_token=" + accessToken;
+
+		String httpString = httpGet(url);
+		JSONObject json = JSONObject.parseObject(httpString);
+
+		OauthUser user = new OauthUser();
+		user.setAvatar(json.getString("avatar_url"));
+		user.setOpenId(json.getString("id"));
+		user.setNickname(json.getString("login"));
+		user.setSource(getName());
+
 		return null;
+	}
+
+	protected String getAccessToken(String code) {
+
+		StringBuilder urlBuilder = new StringBuilder("https://github.com/login/oauth/access_token?");
+		urlBuilder.append("client_id=" + getClientId());
+		urlBuilder.append("&client_secret=" + getClientSecret());
+		urlBuilder.append("&code=" + code);
+
+		String url = urlBuilder.toString();
+
+		String httpString = httpGet(url);
+		JSONObject json = JSONObject.parseObject(httpString);
+		return json.getString("access_token");
 	}
 }

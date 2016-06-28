91e6e8fd41f18061fbfc3db58cb5bd012ee41e02
yangfuhai <fuhai999@163.com>
2016-06-29 00:41:04 +0800

    fix:修复模板文件解压失败无法感知异常的bug

--- jpress/jpress-utils/src/main/java/io/jpress/utils/FileUtils.java
+++ jpress/jpress-utils/src/main/java/io/jpress/utils/FileUtils.java
@@ -101,36 +101,38 @@ public class FileUtils {
 	}
 
 	public static void unzip(String zipFilePath, String targetPath) throws IOException {
-		@SuppressWarnings("resource")
 		ZipFile zipFile = new ZipFile(zipFilePath);
-		Enumeration<?> entryEnum = zipFile.entries();
-		if (null != entryEnum) {
-			while (entryEnum.hasMoreElements()) {
-				OutputStream os = null;
-				InputStream is = null;
-				try {
-					ZipEntry zipEntry = (ZipEntry) entryEnum.nextElement();
-					if (!zipEntry.isDirectory()) {
-						File targetFile = new File(targetPath + File.separator + zipEntry.getName());
-						if (!targetFile.getParentFile().exists()) {
-							targetFile.getParentFile().mkdirs();
+		try{
+			Enumeration<?> entryEnum = zipFile.entries();
+			if (null != entryEnum) {
+				while (entryEnum.hasMoreElements()) {
+					OutputStream os = null;
+					InputStream is = null;
+					try {
+						ZipEntry zipEntry = (ZipEntry) entryEnum.nextElement();
+						if (!zipEntry.isDirectory()) {
+							File targetFile = new File(targetPath + File.separator + zipEntry.getName());
+							if (!targetFile.getParentFile().exists()) {
+								targetFile.getParentFile().mkdirs();
+							}
+							os = new BufferedOutputStream(new FileOutputStream(targetFile));
+							is = zipFile.getInputStream(zipEntry);
+							byte[] buffer = new byte[4096];
+							int readLen = 0;
+							while ((readLen = is.read(buffer, 0, 4096)) > 0) {
+								os.write(buffer, 0, readLen);
+							}
 						}
-						os = new BufferedOutputStream(new FileOutputStream(targetFile));
-						is = zipFile.getInputStream(zipEntry);
-						byte[] buffer = new byte[4096];
-						int readLen = 0;
-						while ((readLen = is.read(buffer, 0, 4096)) > 0) {
-							os.write(buffer, 0, readLen);
-						}
+					} finally {
+						if (is != null)
+							is.close();
+						if (os != null)
+							os.close();
 					}
-				} catch (Exception e) {
-				} finally {
-					if (is != null)
-						is.close();
-					if (os != null)
-						os.close();
 				}
 			}
+		}finally{
+			zipFile.close();
 		}
 	}
 

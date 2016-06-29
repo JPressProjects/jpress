4fefe8f1d4b94a0de7652f027cdb79abfca1f52c
yangfuhai <fuhai999@163.com>
2016-06-29 22:02:17 +0800

    fix:修复有人@你了，email通知无效的bug

--- jpress/jpress-web/src/main/java/io/jpress/listener/AtProcessListener.java
+++ jpress/jpress-web/src/main/java/io/jpress/listener/AtProcessListener.java
@@ -16,31 +16,78 @@
 package io.jpress.listener;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import io.jpress.model.Comment;
+import io.jpress.model.Content;
 import io.jpress.model.User;
+import io.jpress.model.query.OptionQuery;
 import io.jpress.model.query.UserQuery;
+import io.jpress.notify.email.Email;
+import io.jpress.notify.email.EmailSenderFactory;
 import io.jpress.plugin.message.Message;
 import io.jpress.plugin.message.MessageAction;
 import io.jpress.plugin.message.MessageListener;
+import io.jpress.utils.StringUtils;
 
 public class AtProcessListener implements MessageListener {
 
 	@Override
 	public void onMessage(Message message) {
 
+		List<BigInteger> userIds = new ArrayList<BigInteger>();
+
 		// 新增文章
 		if (Actions.CONTENT_ADD.equals(message.getAction())) {
-
+			Content content = message.getData();
+			String text = generateUserLinks(content.getText(), userIds);
+			content.setText(text);
+			content.update();
 		}
 
 		// 新增评论
 		else if (Actions.COMMENT_ADD.equals(message.getAction())) {
-
+			Comment comment = message.getData();
+			String text = generateUserLinks(comment.getText(), userIds);
+			comment.setText(text);
+			comment.update();
 		}
 
+		notifyUser(userIds);
+
+	}
+
+	private void notifyUser(List<BigInteger> userIds) {
+		if (userIds != null && userIds.size() > 0) {
+			for (BigInteger userId : userIds) {
+				notifyByEmail(userId);
+			}
+		}
+	}
+
+	private void notifyByEmail(BigInteger id) {
+		Boolean notify = OptionQuery.findValueAsBool("notify_author_by_email_when_at");
+		if (notify != null && notify == true) {
+			User user = UserQuery.findById(id);
+			if (user == null || user.getEmail() == null) {
+				return;
+			}
+
+			Email email = new Email();
+			email.subject("有人@你了....");
+
+			String content = OptionQuery.findValue("notify_author_content_by_email_when_at");
+			if (!StringUtils.isNotBlank(content)) {
+				content = "有人@你了....";
+			}
+			email.content(content);
+			email.to(user.getEmail());
+
+			EmailSenderFactory.createSender().send(email);
+		}
 	}
 
 	@Override
@@ -49,9 +96,9 @@ public class AtProcessListener implements MessageListener {
 		messageAction.register(Actions.COMMENT_ADD);
 	}
 
-	
 	static Pattern userPattern = Pattern.compile("@([^@^\\s^:]{1,})([\\s\\:\\,\\;]{0,1})");
-	public static String _GenerateRefererLinks(String msg, List<BigInteger> userIds) {
+
+	public static String generateUserLinks(String msg, List<BigInteger> userIds) {
 		StringBuilder html = new StringBuilder();
 		int lastIdx = 0;
 		Matcher matchr = userPattern.matcher(msg);

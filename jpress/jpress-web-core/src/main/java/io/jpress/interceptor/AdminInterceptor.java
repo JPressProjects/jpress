5891b2d42043d98994531003bd1fde14e7652511
yangfuhai <fuhai999@163.com>
2016-08-31 21:24:05 +0800

    fix:修复标签调整后，后台无法编辑的问题

--- jpress/jpress-web-core/src/main/java/io/jpress/interceptor/AdminInterceptor.java
+++ jpress/jpress-web-core/src/main/java/io/jpress/interceptor/AdminInterceptor.java
@@ -17,6 +17,8 @@ package io.jpress.interceptor;
 
 import io.jpress.Consts;
 import io.jpress.menu.MenuManager;
+import io.jpress.model.Content;
+import io.jpress.model.Taxonomy;
 import io.jpress.model.User;
 import io.jpress.utils.EncryptUtils;
 
@@ -25,6 +27,9 @@ import com.jfinal.aop.Invocation;
 import com.jfinal.core.Controller;
 
 public class AdminInterceptor implements Interceptor {
+	
+	static Content null_content = new Content();
+	static Taxonomy null_taxonomy = new Taxonomy();
 
 	@Override
 	public void intercept(Invocation inv) {
@@ -50,6 +55,11 @@ public class AdminInterceptor implements Interceptor {
 		User user = InterUtils.tryToGetUser(inv);
 		
 		if (user != null && user.isAdministrator()) {
+			
+			//content和taxonomy用于清空全局freemarker的全局标签设置，否则会相互冲突
+			controller.setAttr("content", null_content);
+			controller.setAttr("taxonomy", null_taxonomy);
+			
 			controller.setAttr(Consts.ATTR_USER, user);
 			controller.setAttr("ucode", EncryptUtils.generateUcode(user.getId(),user.getSalt()));
 			controller.setAttr("_menu_html", MenuManager.me().generateHtml());
@@ -59,5 +69,6 @@ public class AdminInterceptor implements Interceptor {
 
 		controller.redirect("/admin/login");
 	}
+	
 
 }

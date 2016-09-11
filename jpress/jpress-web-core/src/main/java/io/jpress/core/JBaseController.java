ad82ee01784c8a9cee86bd6278f4499928744785
yangfuhai <fuhai999@163.com>
2016-09-11 21:57:41 +0800

    add:添加是否来之手机浏览器和微信浏览器的判断，方便二次开发。

--- jpress/jpress-web-core/src/main/java/io/jpress/core/JBaseController.java
+++ jpress/jpress-web-core/src/main/java/io/jpress/core/JBaseController.java
@@ -85,6 +85,76 @@ public class JBaseController extends Controller {
 		return "XMLHttpRequest".equalsIgnoreCase(header);
 	}
 
+	static String[] mobileAgents = { "iphone", "android", "phone", "mobile", "wap", "netfront", "java", "opera mobi",
+			"opera mini", "ucweb", "windows ce", "symbian", "series", "webos", "sony", "blackberry", "dopod", "nokia",
+			"samsung", "palmsource", "xda", "pieplus", "meizu", "midp", "cldc", "motorola", "foma", "docomo",
+			"up.browser", "up.link", "blazer", "helio", "hosin", "huawei", "novarra", "coolpad", "webos", "techfaith",
+			"palmsource", "alcatel", "amoi", "ktouch", "nexian", "ericsson", "philips", "sagem", "wellcom", "bunjalloo",
+			"maui", "smartphone", "iemobile", "spice", "bird", "zte-", "longcos", "pantech", "gionee", "portalmmm",
+			"jig browser", "hiptop", "benq", "haier", "^lct", "320x320", "240x320", "176x220", "w3c ", "acs-", "alav",
+			"alca", "amoi", "audi", "avan", "benq", "bird", "blac", "blaz", "brew", "cell", "cldc", "cmd-", "dang",
+			"doco", "eric", "hipt", "inno", "ipaq", "java", "jigs", "kddi", "keji", "leno", "lg-c", "lg-d", "lg-g",
+			"lge-", "maui", "maxo", "midp", "mits", "mmef", "mobi", "mot-", "moto", "mwbp", "nec-", "newt", "noki",
+			"oper", "palm", "pana", "pant", "phil", "play", "port", "prox", "qwap", "sage", "sams", "sany", "sch-",
+			"sec-", "send", "seri", "sgh-", "shar", "sie-", "siem", "smal", "smar", "sony", "sph-", "symb", "t-mo",
+			"teli", "tim-", "tsm-", "upg1", "upsi", "vk-v", "voda", "wap-", "wapa", "wapi", "wapp", "wapr", "webc",
+			"winw", "winw", "xda", "xda-", "googlebot-mobile" };
+
+	/**
+	 * 是否是手机浏览器
+	 * @return
+	 */
+	public boolean isMoblieBrowser() {
+		String ua = getRequest().getHeader("User-Agent");
+		if (ua == null) {
+			return false;
+		}
+		ua = ua.toLowerCase();
+		for (String mobileAgent : mobileAgents) {
+			if (ua.indexOf(mobileAgent) >= 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 是否是微信浏览器
+	 * @return
+	 */
+	public boolean isWechatBrowser() {
+		String ua = getRequest().getHeader("User-Agent");
+		if (ua == null) {
+			return false;
+		}
+		ua = ua.toLowerCase();
+		if (ua.indexOf("micromessenger") > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * 是否是IE浏览器
+	 * @return
+	 */
+	public boolean isIEBrowser() {
+		String ua = getRequest().getHeader("User-Agent");
+		if (ua == null) {
+			return false;
+		}
+
+		ua = ua.toLowerCase();
+		if (ua.indexOf("msie") > 0) {
+			return true;
+		}
+
+		if (ua.indexOf("gecko") > 0 && ua.indexOf("rv:11") > 0) {
+			return true;
+		}
+		return false;
+	}
+
 	public boolean isMultipartRequest() {
 		String contentType = getRequest().getContentType();
 		return contentType != null && contentType.toLowerCase().indexOf("multipart") != -1;
@@ -157,19 +227,6 @@ public class JBaseController extends Controller {
 			renderJson(ar);
 		}
 
-	}
-
-	public boolean isIEBrowser() {
-		String ua = getRequest().getHeader("User-Agent").toLowerCase();
-		if (ua != null && ua.indexOf("msie") > 0) {
-			return true;
-		}
-
-		if (ua != null && ua.indexOf("gecko") > 0 && ua.indexOf("rv:11") > 0) {
-			return true;
-		}
-
-		return false;
 	}
 
 	@Override

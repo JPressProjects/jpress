7902337c3505a25b9e6d4fef683354d75bd81351
yangfuhai <fuhai999@gmail.com>
2018-09-13 14:24:23 +0800

    new jpress

--- jpress-web/src/main/java/io/jpress/web/sharekit/PermissionKits.java
+++ jpress-web/src/main/java/io/jpress/web/sharekit/PermissionKits.java
@@ -1,13 +1,16 @@
 package io.jpress.web.sharekit;
 
 import io.jboot.Jboot;
+import io.jpress.model.User;
 import io.jpress.service.RoleService;
+import io.jpress.web.base.UserInterceptor;
 
 /**
  * @author Michael Yang 杨福海 （fuhai999@gmail.com）
  * @version V1.0
  * @Package io.jpress.core.web.sharekit
  */
+
 public class PermissionKits {
 
     public static boolean hasPermission(long roleId, long permissionId) {
@@ -21,17 +24,17 @@ public class PermissionKits {
         return roleService.hasRole(userId, roleId);
     }
 
-//    public static final boolean hasRole(long roleId) {
-//        User user = UserInterceptor.getThreadLocalUser();
-//        RoleService roleService = Jboot.bean(RoleService.class);
-//        return roleService.hasRole(user.getId(), roleId);
-//    }
+    public static final boolean hasRole(long roleId) {
+        User user = UserInterceptor.getThreadLocalUser();
+        RoleService roleService = Jboot.bean(RoleService.class);
+        return roleService.hasRole(user.getId(), roleId);
+    }
 
-//    public static final boolean hasRole(String roleFlag) {
-//        User user = UserInterceptor.getThreadLocalUser();
-//        RoleService roleService = Jboot.bean(RoleService.class);
-//        return roleService.hasRole(user.getId(), roleFlag);
-//    }
+    public static final boolean hasRole(String roleFlag) {
+        User user = UserInterceptor.getThreadLocalUser();
+        RoleService roleService = Jboot.bean(RoleService.class);
+        return roleService.hasRole(user.getId(), roleFlag);
+    }
 
     public static final boolean hasRole(long userId, String roleFlag) {
         RoleService roleService = Jboot.bean(RoleService.class);

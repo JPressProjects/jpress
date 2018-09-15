10fbb758639a1f074a877e51e39c8b330eccfe4f
yangfuhai <fuhai999@gmail.com>
2018-09-15 11:37:00 +0800

    new jpress

---
+++ jpress-web/src/main/java/io/jpress/web/admin/_AdminCommonsController.java
@@ -0,0 +1,38 @@
+package io.jpress.web.admin;
+
+import com.github.stuxuhai.jpinyin.PinyinException;
+import com.github.stuxuhai.jpinyin.PinyinFormat;
+import com.github.stuxuhai.jpinyin.PinyinHelper;
+import com.jfinal.kit.Ret;
+import io.jboot.utils.StringUtils;
+import io.jboot.web.controller.annotation.RequestMapping;
+import io.jpress.web.base.AdminControllerBase;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Title: 首页
+ * @Package io.jpress.web.admin
+ */
+@RequestMapping("/admin/commons")
+public class _AdminCommonsController extends AdminControllerBase {
+
+
+    public void doGetPinyin() {
+        String para = getPara();
+        if (StringUtils.isBlank(para)) {
+            renderJson(Ret.fail());
+            return;
+        }
+
+        try {
+            String pinyin = PinyinHelper.convertToPinyinString(StringUtils.urlDecode(para), "", PinyinFormat.WITHOUT_TONE);
+            renderJson(Ret.ok().set("data", pinyin));
+            return;
+        } catch (PinyinException e) {
+            e.printStackTrace();
+        }
+
+        renderJson(Ret.fail());
+    }
+}

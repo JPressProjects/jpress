0f4fc60a0ef8f6c8e79e330941793db831cb47ed
yangfuhai <fuhai999@gmail.com>
2018-09-27 16:12:41 +0800

    new jpress

--- starter-tomcat/src/main/java/org/springframework/web/context/ContextLoaderListener.java
+++ starter-tomcat/src/main/java/org/springframework/web/context/ContextLoaderListener.java
@@ -10,10 +10,12 @@ import javax.servlet.ServletContextListener;
  * 从而造成tomcat找不到 ContextLoaderListener ， 无法启动的情况
  * <p>
  * 解决方案有2：
- * 1、排除dubbo，但是已有若是用到 dubbo 还是会出现这样的问题
+ * 1、排除dubbo，但是以后若是用到 dubbo 还是会出现这样的问题
  * 2、写此类，做一个空实现。
- *
- * 另：已经向dubbo官方提出issue，等待解决：https://github.com/apache/incubator-dubbo/issues/2570
+ * <p>
+ * 另：
+ * 已经向dubbo官方提出issue 并 给出解决方案：https://github.com/apache/incubator-dubbo/issues/2570
+ * 等待 dubbo 官方解决，解决之后会删除此类。
  */
 public class ContextLoaderListener implements ServletContextListener {
     @Override

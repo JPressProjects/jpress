01223fceaf4de8b0ec30916ce587df13e67af318
yangfuhai <fuhai999@gmail.com>
2018-09-30 13:12:18 +0800

    new jpress

--- jpress-web/src/main/resources/static/commons/jpresscommons.js
+++ jpress-web/src/main/resources/static/commons/jpresscommons.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
 
-
+    initJPActions();
     initToastr();
     initSwitchery();
-
-
+    initDomainSpan();
+    initSlugSpan();
 });
 
+function initJPActions() {
+    $(".jp-actiontr").mouseover(function () {
+        $(this).find(".jp-actionitem").show();
+    }).mouseout(function () {
+        $(".jp-actionitem").hide()
+    })
+}
 
 function initToastr() {
     if (typeof toastr != "undefined") {
@@ -36,6 +43,65 @@ function initSwitchery(config) {
     });
 
 }
+
+function initDomainSpan() {
+    $(".domainSpan").each(function () {
+        if ($(this).text() == "") {
+            $(this).text(window.location.protocol + "//" + window.location.host)
+        }
+    })
+}
+
+
+function initSlugSpan() {
+
+    var reg = /([\s|\。|\，|\？|\、|\"|\“|\”|\‘|\'|\（|\）|\《|\》|\… |\～|\￥|\&|\*|\@|\#|\$||\%|\`|\.|\【|\】|\-])+/ig;
+
+    String.prototype.endWith = function (s) {
+        if (s == null || s == "" || this.length == 0 || s.length > this.length)
+            return false;
+        if (this.substring(this.length - s.length) == s)
+            return true;
+        else
+            return false;
+    }
+
+    $(".slugSpan").each(function () {
+
+        var that = $(this);
+
+        var forListener = that.attr("for-listener");
+        var forInput = that.attr("for-input");
+
+        $(this).editable({
+            emptytext: "标题"
+        });
+
+        $(this).on('save', function (e, params) {
+            $('#' + forInput).attr('value', params.newValue);
+        });
+
+
+        $("#" + forListener).keyup(function () {
+            if ($('#' + forInput).val() == "") {
+
+                var value = this.value.replace(reg, "_");
+                if (value.endWith("_")) {
+                    value = value.substring(0, value.length - 1);
+                }
+
+                $.get("/commons/pinyin/doGetPinyin/" + value, function (result) {
+                    if ("ok" == result.state) {
+                        var pinyin = result.data;
+                        that.text(pinyin);
+                        that.editable('setValue', pinyin);
+                    }
+                });
+            }
+        });
+    })
+}
+
 
 
 function getPara(variable) {

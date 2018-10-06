1ac9f2031229fb2dfd5ae623da905463b571de28
yangfuhai <fuhai999@gmail.com>
2018-10-06 15:15:28 +0800

    new jpress

--- module-article/module-article-web/src/main/java/io/jpress/module/article/directive/ArticleCategoriesDirective.java
+++ module-article/module-article-web/src/main/java/io/jpress/module/article/directive/ArticleCategoriesDirective.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jpress.module.article.directive;
 
 import com.jfinal.template.Env;
@@ -37,18 +52,6 @@ public class ArticleCategoriesDirective extends JbootDirectiveBase {
         if (categories == null || categories.isEmpty()) {
             return;
         }
-//        try {
-//            for (int i = 0; i < categories.size(); i++) {
-//                ArticleCategory category = categories.get(i);
-//                if (i == categories.size() - 1) {
-//                    writer.write(String.format("<a href=\"\">%s</a>", category.getTitle()));
-//                } else {
-//                    writer.write(String.format("<a href=\"\">%s</a> , ", category.getTitle()));
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         scope.setLocal("categories", categories);
         renderBody(env, scope, writer);
     }

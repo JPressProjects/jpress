14e8f348ee3f439e53172cd976c7ed7cc37f2243
yangfuhai <fuhai999@gmail.com>
2018-10-06 14:11:13 +0800

    new jpress

--- jpress-model/src/main/java/io/jpress/model/User.java
+++ jpress-model/src/main/java/io/jpress/model/User.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jpress.model;
 
 import io.jboot.db.annotation.Table;
@@ -43,7 +58,7 @@ public class User extends BaseUser<User> {
     public void keepUpdateSafe() {
 
         //这些字段不允许用户自己更新
-        
+
         remove("password", "salt",
                 "username", "wx_openid",
                 "wx_unionid", "qq_openid",

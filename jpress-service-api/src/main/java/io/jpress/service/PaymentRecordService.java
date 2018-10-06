953ec65a215d5bf5d4af2905b94b1896feb8b8bf
yangfuhai <fuhai999@gmail.com>
2018-10-06 14:12:20 +0800

    new jpress

--- jpress-service-api/src/main/java/io/jpress/service/PaymentRecordService.java
+++ jpress-service-api/src/main/java/io/jpress/service/PaymentRecordService.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jpress.service;
 
 import com.jfinal.plugin.activerecord.Model;
@@ -6,7 +21,7 @@ import io.jpress.model.PaymentRecord;
 
 import java.util.List;
 
-public interface PaymentRecordService  {
+public interface PaymentRecordService {
 
     /**
      * find model by primary key
@@ -71,18 +86,30 @@ public interface PaymentRecordService  {
 
 
     public void join(Page<? extends Model> page, String joinOnField);
+
     public void join(Page<? extends Model> page, String joinOnField, String[] attrs);
+
     public void join(Page<? extends Model> page, String joinOnField, String joinName);
+
     public void join(Page<? extends Model> page, String joinOnField, String joinName, String[] attrs);
+
     public void join(List<? extends Model> models, String joinOnField);
+
     public void join(List<? extends Model> models, String joinOnField, String[] attrs);
+
     public void join(List<? extends Model> models, String joinOnField, String joinName);
+
     public void join(List<? extends Model> models, String joinOnField, String joinName, String[] attrs);
+
     public void join(Model model, String joinOnField);
+
     public void join(Model model, String joinOnField, String[] attrs);
+
     public void join(Model model, String joinOnField, String joinName);
+
     public void join(Model model, String joinOnField, String joinName, String[] attrs);
 
     public void keep(Model model, String... attrs);
+
     public void keep(List<? extends Model> models, String... attrs);
 }

0058facdc6f3b5f2c69df559d3919ece5700ce98
yangfuhai <fuhai999@gmail.com>
2018-10-06 15:10:49 +0800

    new jpress

--- jpress-web/src/main/java/io/jpress/web/wechat/WechatAuthorizationController.java
+++ jpress-web/src/main/java/io/jpress/web/wechat/WechatAuthorizationController.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jpress.web.wechat;
 
 import io.jboot.web.controller.annotation.RequestMapping;
@@ -12,7 +27,7 @@ import io.jpress.web.base.ControllerBase;
 public class WechatAuthorizationController extends ControllerBase {
 
 
-    public void index(){
+    public void index() {
 
     }
 

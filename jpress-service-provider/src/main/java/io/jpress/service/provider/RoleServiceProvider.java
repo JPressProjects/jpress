3bb54e2724c7f747c9b3898c1afdbb1247211eb7
yangfuhai <fuhai999@gmail.com>
2018-10-06 14:14:04 +0800

    new jpress

--- jpress-service-provider/src/main/java/io/jpress/service/provider/RoleServiceProvider.java
+++ jpress-service-provider/src/main/java/io/jpress/service/provider/RoleServiceProvider.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jpress.service.provider;
 
 import com.jfinal.plugin.activerecord.Db;
@@ -6,11 +21,8 @@ import io.jboot.Jboot;
 import io.jboot.aop.annotation.Bean;
 import io.jboot.core.cache.annotation.CacheEvict;
 import io.jboot.core.cache.annotation.Cacheable;
-import io.jboot.db.model.Column;
-import io.jboot.db.model.Columns;
 import io.jboot.service.JbootServiceBase;
 import io.jpress.commons.utils.SqlUtils;
-import io.jpress.model.Permission;
 import io.jpress.model.Role;
 import io.jpress.service.RoleService;
 

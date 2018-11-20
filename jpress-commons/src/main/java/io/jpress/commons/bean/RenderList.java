735cbc1e712b17b9e4deb9c97076d22c30fe3f77
yangfuhai <fuhai999@gmail.com>
2018-11-20 17:28:56 +0800

    fixed：优化 linesOption 指令

---
+++ jpress-commons/src/main/java/io/jpress/commons/bean/RenderList.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jpress.commons.bean;
+
+import java.util.ArrayList;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Title: 用于在模板进行渲染，防止出错 get(index) 出错
+ * @Package io.jpress.commons.bean
+ */
+public class RenderList<E> extends ArrayList<E> {
+
+    @Override
+    public E get(int index) {
+        if (index >= size()) return null;
+        return super.get(index);
+    }
+}

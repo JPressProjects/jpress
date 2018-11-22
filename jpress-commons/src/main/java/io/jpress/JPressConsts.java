86c23099d21334f59913f01edbbf9c3cfa051174
yangfuhai <fuhai999@gmail.com>
2018-11-22 15:00:14 +0800

    v1.0-rc.8 release

--- jpress-commons/src/main/java/io/jpress/JPressConsts.java
+++ jpress-commons/src/main/java/io/jpress/JPressConsts.java
@@ -25,7 +25,7 @@ import com.jfinal.plugin.activerecord.Model;
  */
 public class JPressConsts {
 
-    public static final String VERSION = "v1.0-rc.7";
+    public static final String VERSION = "v1.0-rc.8";
 
     /**
      * 后台系统菜单的 ID

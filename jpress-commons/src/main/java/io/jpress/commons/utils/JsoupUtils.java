c50ac388314c55bdaa28f1e554c575902ad68ad6
fuhai999@gmail.com <fuhai999@gmail.com>
2019-01-10 20:40:51 +0800

    v1.0.4

--- jpress-commons/src/main/java/io/jpress/commons/utils/JsoupUtils.java
+++ jpress-commons/src/main/java/io/jpress/commons/utils/JsoupUtils.java
@@ -110,9 +110,10 @@ public class JsoupUtils {
      * @author michael
      */
     public static class MyWhitelist extends org.jsoup.safety.Whitelist {
+
         public MyWhitelist() {
 
-            addTags("a", "b", "blockquote", "br", "caption", "cite", "code", "col", "colgroup", "dd", "div", "dl", "dt",
+            addTags("a", "b", "blockquote", "br", "caption", "cite", "code", "col", "colgroup", "dd", "div", "span", "embed", "object", "dl", "dt",
                     "em", "h1", "h2", "h3", "h4", "h5", "h6", "i", "img", "li", "ol", "p", "pre", "q", "small",
                     "strike", "strong", "sub", "sup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "u", "ul");
 
@@ -127,27 +128,34 @@ public class JsoupUtils {
             addAttributes("td", "abbr", "axis", "colspan", "rowspan", "width");
             addAttributes("th", "abbr", "axis", "colspan", "rowspan", "scope", "width");
             addAttributes("video", "src", "autoplay", "controls", "loop", "muted", "poster", "preload");
+            addAttributes("object", "width", "height", "classid", "codebase");
+            addAttributes("param", "name", "value");
+            addAttributes("embed", "src", "quality", "width", "height", "allowFullScreen", "allowScriptAccess", "flashvars", "name", "type", "pluginspage");
 
-            addAttributes(":all", "class");
-            addAttributes(":all", "style");
-            addAttributes(":all", "height");
-            addAttributes(":all", "width");
-            addAttributes(":all", "type");
-            addAttributes(":all", "id");
-            addAttributes(":all", "name");
+            addAttributes(":all", "class", "style", "height", "width", "type", "id", "name");
 
             addProtocols("a", "href", "ftp", "http", "https", "mailto", "tel");
             addProtocols("blockquote", "cite", "http", "https");
             addProtocols("cite", "cite", "http", "https");
-            addProtocols("img", "src", "http", "https");
             addProtocols("q", "cite", "http", "https");
+
+            //如果添加以下的协议，那么src必须是http 或者 https 开头，相对路径则被过滤掉了，
+            //所以必须注释掉，运行相对路径的图片资源
+            //addProtocols("img", "src", "http", "https");
         }
 
         @Override
         protected boolean isSafeAttribute(String tagName, Element el, Attribute attr) {
+            if ("src".equalsIgnoreCase(attr.getKey())) {
+                String src = attr.getValue();
+                if (StrUtils.isNotBlank(src) && src.toLowerCase().startsWith("javascript")) {
+                    return false;
+                }
+            }
             return ("img".equals(tagName) && "src".equals(attr.getKey()) && attr.getValue().startsWith("data:;base64"))
                     || super.isSafeAttribute(tagName, el, attr);
         }
     }
+
 
 }

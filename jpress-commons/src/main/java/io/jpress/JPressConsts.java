e18b8ef24232e29b0f07cefb719d631f5790552c
fuhai999@gmail.com <fuhai999@gmail.com>
2019-01-14 15:37:49 +0800

    v1.0.4

--- jpress-commons/src/main/java/io/jpress/JPressConsts.java
+++ jpress-commons/src/main/java/io/jpress/JPressConsts.java
@@ -25,7 +25,7 @@ import com.jfinal.plugin.activerecord.Model;
  */
 public class JPressConsts {
 
-    public static final String VERSION = "v1.0.3";
+    public static final String VERSION = "v1.0.4";
 
     /**
      * 后台系统菜单的 ID

1985f8ea3cfcd612ce3a3c945531659756ceac33
fuhai999@gmail.com <fuhai999@gmail.com>
2019-01-16 11:20:58 +0800

    v2.0 rc.1

---
+++ starter/src/main/java/io/jpress/PathKitIniter.java
@@ -0,0 +1,11 @@
+package io.jpress;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Title: (请输入文件名称)
+ * @Description: (用一句话描述该文件做什么)
+ * @Package io.jpress
+ */
+public class PathKitIniter {
+}

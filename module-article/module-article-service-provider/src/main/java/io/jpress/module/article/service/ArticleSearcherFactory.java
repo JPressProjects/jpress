6668267482282c2cd9a28ad2a5352ca2e7a33d44
fuhai999@gmail.com <fuhai999@gmail.com>
2019-01-28 13:17:07 +0800

    v2.0-rc.1

--- module-article/module-article-service-provider/src/main/java/io/jpress/module/article/service/ArticleSearcherFactory.java
+++ module-article/module-article-service-provider/src/main/java/io/jpress/module/article/service/ArticleSearcherFactory.java
@@ -2,12 +2,36 @@ package io.jpress.module.article.service;
 
 
 import com.jfinal.aop.Aop;
+import io.jboot.core.spi.JbootSpiLoader;
+import io.jboot.utils.StrUtil;
+import io.jpress.JPressOptions;
 import io.jpress.module.article.searcher.DbSearcher;
+import io.jpress.module.article.searcher.LuceneSearcher;
 import io.jpress.module.article.service.search.ArticleSearcher;
 
 public class ArticleSearcherFactory {
 
     public static ArticleSearcher getSearcher() {
-        return Aop.get(DbSearcher.class);
+
+        String engine = JPressOptions.get("article_search_engine");
+
+        if (StrUtil.isBlank(engine)) {
+            return Aop.get(DbSearcher.class);
+        }
+
+        switch (engine) {
+            case "sql":
+                return Aop.get(DbSearcher.class);
+            case "lucene":
+                return Aop.get(LuceneSearcher.class);
+            case "es":
+                return Aop.get(LuceneSearcher.class);
+            case "aliopensearch":
+                return Aop.get(LuceneSearcher.class);
+        }
+
+        ArticleSearcher searcher = JbootSpiLoader.load(ArticleSearcher.class, engine);
+        return searcher != null ? searcher : Aop.get(DbSearcher.class);
+
     }
 }

8cbf638309042a86d47c9e32766297d0c6dac46b
fuhai999@gmail.com <fuhai999@gmail.com>
2019-01-29 15:29:24 +0800

    v2.0-rc.1

--- jpress-addons/jpress-addon-helloworld/src/main/java/io/jpress/addon/helloworld/HelloWorldAddon.java
+++ jpress-addons/jpress-addon-helloworld/src/main/java/io/jpress/addon/helloworld/HelloWorldAddon.java
@@ -8,26 +8,26 @@ public class HelloWorldAddon implements Addon {
     @Override
     public void onInstall() {
 
-        System.out.println("onInstall");
+        System.out.println("HelloWorldAddon onInstall");
 
     }
 
     @Override
     public void onUninstall() {
 
-        System.out.println("onUninstall");
+        System.out.println("HelloWorldAddon onUninstall");
     }
 
     @Override
     public void onStart() {
 
-        System.out.println("onStart");
+        System.out.println("HelloWorldAddon onStart");
     }
 
     @Override
     public void onStop() {
 
-        System.out.println("onStop");
+        System.out.println("HelloWorldAddon onStop");
 
     }
 }

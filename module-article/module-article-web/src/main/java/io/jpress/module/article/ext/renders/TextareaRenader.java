a5ad91a8234a735dbb141c8c89c1f62fb483df93
fuhai999@gmail.com <fuhai999@gmail.com>
2019-02-16 17:43:42 +0800

    v2.0-rc.3

---
+++ module-article/module-article-web/src/main/java/io/jpress/module/article/ext/renders/TextareaRenader.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jpress.module.article.ext.renders;
+
+import io.jpress.module.article.ext.ArticleField;
+import io.jpress.module.article.ext.ArticleFieldRender;
+import io.jpress.module.article.model.Article;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Title: Textarea 的渲染器
+ */
+public class TextareaRenader implements ArticleFieldRender {
+    
+    @Override
+    public String onRender(Article article, ArticleField field) {
+        return null;
+    }
+}

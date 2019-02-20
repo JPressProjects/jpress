99d12305d6fa018d8f483df402f094711e31c281
fuhai999@gmail.com <fuhai999@gmail.com>
2019-02-20 16:30:11 +0800

    v2.0-rc.3

--- jpress-web/src/main/webapp/static/commons/jpresscommons.js
+++ jpress-web/src/main/webapp/static/commons/jpresscommons.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
 
 function initDatepicker() {
     $('.datepicker').datepicker({
-        format: $(this).attr("data-format"),
         language: 'zh-CN'
     });
 }

f5dbe36d7bc6dc17f5d222383a2e092a3638f82f
fuhai999@gmail.com <fuhai999@gmail.com>
2019-02-28 11:47:39 +0800

    v2.0-rc.4

---
+++ module-article/module-article-web/src/main/java/io/jpress/module/article/directive/CommentsDirective.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jpress.module.article.directive;
+
+import com.jfinal.aop.Inject;
+import com.jfinal.template.Env;
+import com.jfinal.template.io.Writer;
+import com.jfinal.template.stat.Scope;
+import io.jboot.db.model.Columns;
+import io.jboot.web.directive.annotation.JFinalDirective;
+import io.jboot.web.directive.base.JbootDirectiveBase;
+import io.jpress.module.article.model.ArticleComment;
+import io.jpress.module.article.service.ArticleCommentService;
+
+import java.util.List;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Package io.jpress.module.page.directive
+ */
+@JFinalDirective("comments")
+public class CommentsDirective extends JbootDirectiveBase {
+
+    @Inject
+    private ArticleCommentService service;
+
+
+    @Override
+    public void onRender(Env env, Scope scope, Writer writer) {
+
+        String orderBy = getPara("orderBy", scope, "id desc");
+        int count = getParaToInt("count", scope, 10);
+
+        Columns columns = Columns.create("status", ArticleComment.STATUS_NORMAL);
+        List<ArticleComment> comments = service.findListByColumns(columns, orderBy, count);
+
+        if (comments == null || comments.isEmpty()) {
+            return;
+        }
+
+        scope.setLocal("comments", comments);
+        renderBody(env, scope, writer);
+    }
+
+
+    @Override
+    public boolean hasEnd() {
+        return true;
+    }
+}

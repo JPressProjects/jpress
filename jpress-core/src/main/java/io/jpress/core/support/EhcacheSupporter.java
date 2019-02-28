e6b65b552d77488e86276fc91d28c7c1ea138d99
fuhai999@gmail.com <fuhai999@gmail.com>
2019-02-28 12:20:54 +0800

    v2.0-rc.4

--- jpress-core/src/main/java/io/jpress/core/support/EhcacheSupporter.java
+++ jpress-core/src/main/java/io/jpress/core/support/EhcacheSupporter.java
@@ -30,11 +30,12 @@ import java.util.concurrent.ConcurrentHashMap;
  * EhcacheSupporter 的主要作用是用于初始化 EhCache 的 CacheManager
  * 默认情况下，Ehcache 是通过 默认的 Classloader 加载数据的
  * 但是由于 JPress 内置了插件机制，所有的插件都是通过插件自己的 Classloader 进行加载
- * 这样会导致 EhCache 和 插件的 Classloader 不是通过一个 Classloader，从而导致 ClassNotFound 的异常
+ * 这样会导致 EhCache 和 插件的 Classloader 不是通过一个 Classloader，当插件使用ehcache缓存的时候，
+ * 就会导致 ClassNotFound 的异常出现
  * <p>
  * 所以，此类的主要作用，是保证 EhCache 用于加载缓存的 Classloader 能够找到 插件加载进来的 Class
- *
- * 另外：对于插件来说，每个插件必须使用一个新的 Classloader，才能保证 插件在后台进行 安装、卸载、停止、启用的正常工作
+ * <p>
+ * 另外：对于插件来说，每个插件必须使用自己的 Classloader，才能保证 插件在后台进行 安装、卸载、停止、启用的正常工作
  * 否则当用户卸载插件后重新安装，无法加载到新的Class（之前的Class还在内存里）
  */
 public class EhcacheSupporter {
@@ -63,20 +64,23 @@ public class EhcacheSupporter {
 
     public static class EhcacheClassloader extends ClassLoader {
 
-        private final static ClassLoader parent = EhcacheClassloader.class.getClassLoader();
-        private final static Map<String, ClassLoader> classLoaderCache = new ConcurrentHashMap<>();
-
-        public EhcacheClassloader() {
-        }
+        private ClassLoader parent = EhcacheClassloader.class.getClassLoader();
+        private Map<String, ClassLoader> classLoaderCache = null;
 
 
-        public void addMapping(String className, ClassLoader classLoader) {
+        public synchronized void addMapping(String className, ClassLoader classLoader) {
+            if (classLoaderCache == null){
+                classLoaderCache = new ConcurrentHashMap<>();
+            }
             classLoaderCache.put(className, classLoader);
         }
 
 
         @Override
         public Class<?> loadClass(String name) throws ClassNotFoundException {
+            if (classLoaderCache == null || classLoaderCache.isEmpty()){
+                return parent.loadClass(name);
+            }
             ClassLoader c = classLoaderCache.get(name);
             if (c == null) c = parent;
             return c.loadClass(name);

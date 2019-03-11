2198e894d8f6600e5e2c6b5305e8eb9437c5527c
ninemm <ninemm@126.com>
2019-03-11 10:35:01 +0800

    feature: 添加日期工具类

---
+++ jpress-commons/src/main/java/io/jpress/commons/utils/DateUtils.java
@@ -0,0 +1,172 @@
+package io.jpress.commons.utils;
+
+import com.google.common.collect.Lists;
+import io.jboot.utils.StrUtil;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 日期工具类
+ *
+ * @author Eric.Huang
+ * @date 2019-03-10 10:37
+ * @package io.jpress.commons.utils
+ **/
+
+public class DateUtils {
+
+    public static final String DEFAULT_YEAR_MONTH_DAY = "yyyy-MM-dd";
+
+    /** 缺省日期格式,精确到秒 */
+    public static final String DEFAULT_FORMATTER = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 功能描述：字符串转日期
+     * @param str		日期字符串
+     * @param format	日期格式
+     * @return
+     * 返回类型：Date
+     * 创建人：eric
+     * 日期：2017年2月16日
+     */
+    public static Date strToDate(String str, String format) {
+
+        if (StrUtil.isBlank(str)) {
+            return null;
+        }
+
+        if (StrUtil.isBlank(format)) {
+            format = DEFAULT_FORMATTER;
+        }
+
+        DateTimeFormatter dtf = DateTimeFormat.forPattern(format);
+        return dtf.parseDateTime(str).toDate();
+
+    }
+
+    public static List<DateTime> getAllDaysBeforeExpireDate(Date expireDate) {
+        if (expireDate == null) {
+            return null;
+        }
+
+        List<DateTime> result = Lists.newArrayList();
+        DateTime endDateTime = new DateTime(expireDate);
+        List<DateTime> list = getAllDaysInMonth();
+
+        for (DateTime dateTime : list) {
+            if (dateTime.isBefore(endDateTime)) {
+                result.add(dateTime);
+            }
+        }
+
+        return result;
+    }
+
+    /** 获取当前月的剩余天数 */
+    public static List<DateTime> getAllDaysInMonth() {
+
+        DateTime now = DateTime.now();
+        int curDay = now.getDayOfMonth();
+        int maxDay = now.dayOfMonth().getMaximumValue();
+        List<DateTime> result = Lists.newArrayList();
+
+        for (int i = curDay + 1; i <= maxDay; i++) {
+            result.add(now.withDayOfYear(i));
+        }
+        return result;
+    }
+
+    /** 获取一年中的剩余天数 */
+    public static List<DateTime> getAllDaysInYear() {
+
+        DateTime now = DateTime.now();
+        int curDay = now.getDayOfYear();
+        int maxDay = now.dayOfYear().getMaximumValue();
+        List<DateTime> result = Lists.newArrayList();
+
+        for (int i = curDay + 1; i <= maxDay; i++) {
+            result.add(now.withDayOfYear(i));
+        }
+        return result;
+    }
+
+    /**
+     * 功能描述：获取当前时间之后的所有周几日期，如每周二
+     * 输入参数：
+     * @param weekNo	周几，如周一，周二
+     * @return
+     * 返回类型：List<DateTime>
+     * 创建人：eric
+     * 日期：2017年6月13日
+     */
+    public static List<DateTime> dayOfWeek(int weekNo) {
+
+        DateTime now = DateTime.now();
+        DateTime dateTime = null;
+        int week = now.getDayOfWeek();
+        if (week > weekNo) {
+            dateTime = now.plusWeeks(1).withDayOfWeek(weekNo);
+        } else {
+            dateTime = now.withDayOfWeek(weekNo);
+        }
+
+        List<DateTime> list = Lists.newLinkedList();
+
+        // 获取当前周是当年内第几周
+        int currentWeekNo = dateTime.getWeekOfWeekyear();
+        // 获取当年的总周数
+        int maxWeekNo = dateTime.weekOfWeekyear().getMaximumValue();
+
+        for ( int i = currentWeekNo; i <= maxWeekNo; i++) {
+            list.add(new DateTime().withWeekOfWeekyear(i).withDayOfWeek(weekNo));
+        }
+
+        return list;
+    }
+
+    /**
+     * 功能描述：给指定日期添加天数
+     * @param date	日期
+     * @param day	天数
+     * @return
+     * 返回类型：Date
+     * 创建人：eric
+     * 日期：2017年6月10日
+     */
+    public static Date plusDays(Date date, int day) {
+        DateTime dateTime = new DateTime(date);
+        return dateTime.plusDays(day).toDate();
+    }
+
+    /**
+     * 功能描述：获取当前时间之后，指定时间之前的所有周几日期，如每周二
+     * @param weekNo		周几，如周一，周二
+     * @param endDate		截至日期
+     * @return
+     * 返回类型：List<DateTime>
+     * 创建人：eric
+     * 日期：2017年6月19日
+     */
+    public static List<DateTime> dayOfWeek(int weekNo, Date endDate) {
+
+        if (endDate == null) {
+            return null;
+        }
+
+        List<DateTime> result = Lists.newArrayList();
+        DateTime endDateTime = new DateTime(endDate);
+        List<DateTime> list = dayOfWeek(weekNo);
+
+        for (DateTime dateTime : list) {
+            if (dateTime.isBefore(endDateTime)) {
+                result.add(dateTime);
+            }
+        }
+
+        return result;
+    }
+}

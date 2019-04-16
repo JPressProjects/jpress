75689d808d5127f45f459fda666b4ab2ad91e0b1
fuhai999@gmail.com <fuhai999@gmail.com>
2019-04-16 16:25:49 +0800

    v2.0-rc.9

--- jpress-core/src/main/java/io/jpress/web/directive/CheckedIfDirective.java
+++ jpress-core/src/main/java/io/jpress/web/directive/CheckedIfDirective.java
@@ -16,13 +16,10 @@
 package io.jpress.web.directive;
 
 import com.jfinal.template.Env;
-import com.jfinal.template.TemplateException;
 import com.jfinal.template.io.Writer;
 import com.jfinal.template.stat.Scope;
 import io.jboot.web.directive.annotation.JFinalDirective;
 import io.jboot.web.directive.base.JbootDirectiveBase;
-
-import java.io.IOException;
 
 /**
  * @author Michael Yang 杨福海 （fuhai999@gmail.com）
@@ -32,17 +29,17 @@ import java.io.IOException;
 @JFinalDirective("checkedIf")
 public class CheckedIfDirective extends JbootDirectiveBase {
 
-
     @Override
     public void onRender(Env env, Scope scope, Writer writer) {
         Object param = getPara(0, scope);
-        if ("true".equalsIgnoreCase(String.valueOf(param))) {
-            try {
-                writer.write("checked");
-            } catch (IOException e) {
-                throw new TemplateException(e.getMessage(), location, e);
+        if (param instanceof Boolean) {
+            if ((Boolean) param) {
+                renderText(writer,"checked");
             }
+        } else if ("true".equalsIgnoreCase(String.valueOf(param))) {
+            renderText(writer,"checked");
         }
     }
+
 }
 

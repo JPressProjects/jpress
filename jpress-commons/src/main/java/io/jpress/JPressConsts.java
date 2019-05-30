20338b3bb3cd324f0a6d6bf3ce4c8ccbcf2a4330
fuhai999@gmail.com <fuhai999@gmail.com>
2019-05-30 14:34:34 +0800

    v2.0.2 release (^.^)YYa!!

--- jpress-commons/src/main/java/io/jpress/JPressConsts.java
+++ jpress-commons/src/main/java/io/jpress/JPressConsts.java
@@ -23,7 +23,7 @@ package io.jpress;
  */
 public class JPressConsts {
 
-    public static final String VERSION = "v2.0.1";
+    public static final String VERSION = "v2.0.2";
 
     /**
      * 后台系统菜单的 ID

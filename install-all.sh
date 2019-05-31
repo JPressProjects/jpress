6788813cb9ba6a47d3188971df855a4cd6853990
fuhai999@gmail.com <fuhai999@gmail.com>
2019-05-31 15:30:59 +0800

    v2.0.3

---
+++ install-all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# ----------------------------------------------------------------------
+# author:       yangfuhai
+# email:        fuhai999@gmail.com
+# use : yum install -y wget && wget -O install.sh https://gitee.com/fuhai/jpress/raw/master/install-all.sh && bash install.sh
+# ----------------------------------------------------------------------
+
+
+# 安装docker
+sudo yum update
+echo y | sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum list docker-ce --showduplicates | sort -r
+echo y | sudo yum install docker-ce
+
+# 启动docker和开机自启动
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# 查看版本
+docker version
+
+# 安装docker-compose
+pip install --upgrade pip
+pip install docker-compose
+docker-compose -version
+
+# 安装并运行jpress
+wget https://gitee.com/fuhai/jpress/raw/master/docker-compose.yml
+docker-compose up -d
+
+
+
+
+
+
+

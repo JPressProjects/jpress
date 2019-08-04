a61e13f8f140a0e24342a4073cac68edd7030481
fuhai999@gmail.com <fuhai999@gmail.com>
2019-08-04 12:17:19 +0800

    v2.0.7

--- install-docker.sh
+++ install-docker.sh
@@ -7,7 +7,7 @@
 
 
 # 安装docker
-sudo yum update
+# sudo yum update
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum list docker-ce --showduplicates | sort -r

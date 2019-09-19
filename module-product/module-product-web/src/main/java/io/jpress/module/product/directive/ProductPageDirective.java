f56f82513daf014bd5028a7639f36f12bbe5f0e3
fuhai999@gmail.com <fuhai999@gmail.com>
2019-09-19 13:52:06 +0800

    jpress3

---
+++ module-product/module-product-web/src/main/java/io/jpress/module/product/directive/ProductPageDirective.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jpress.module.product.directive;
+
+import com.jfinal.aop.Inject;
+import com.jfinal.core.Controller;
+import com.jfinal.core.JFinal;
+import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.template.Env;
+import com.jfinal.template.io.Writer;
+import com.jfinal.template.stat.Scope;
+import io.jboot.web.controller.JbootControllerContext;
+import io.jboot.web.directive.annotation.JFinalDirective;
+import io.jboot.web.directive.base.JbootDirectiveBase;
+import io.jboot.web.directive.base.PaginateDirectiveBase;
+import io.jpress.JPressOptions;
+import io.jpress.commons.directive.DirectveKit;
+import io.jpress.module.product.model.Product;
+import io.jpress.module.product.model.ProductCategory;
+import io.jpress.module.product.service.ProductService;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Package io.jpress.module.page.directive
+ */
+@JFinalDirective("productPage")
+public class ProductPageDirective extends JbootDirectiveBase {
+
+    @Inject
+    private ProductService service;
+
+    @Override
+    public void onRender(Env env, Scope scope, Writer writer) {
+
+        Controller controller = JbootControllerContext.get();
+
+        int page = controller.getParaToInt(1, 1);
+        int pageSize = getParaToInt("pageSize", scope, 10);
+        String orderBy = getPara("orderBy", scope, "id desc");
+
+        // 可以指定当前的分类ID
+        Long categoryId = getParaToLong("categoryId", scope, 0L);
+        ProductCategory category = controller.getAttr("category");
+
+        if (categoryId == 0 && category != null) {
+            categoryId = category.getId();
+        }
+
+        Page<Product> productPage = categoryId == 0
+                ? service.paginateInNormal(page, pageSize, orderBy)
+                : service.paginateByCategoryIdInNormal(page, pageSize, categoryId, orderBy);
+
+        scope.setGlobal("productPage", productPage);
+        renderBody(env, scope, writer);
+    }
+
+    @Override
+    public boolean hasEnd() {
+        return true;
+    }
+
+
+    @JFinalDirective("productPaginate")
+    public static class TemplatePaginateDirective extends PaginateDirectiveBase {
+
+        private boolean firstGotoIndex = false;
+
+        @Override
+        public void onRender(Env env, Scope scope, Writer writer) {
+            firstGotoIndex = getPara("firstGotoIndex", scope, false);
+            super.onRender(env, scope, writer);
+        }
+
+        @Override
+        protected String getUrl(int pageNumber) {
+            HttpServletRequest request = JbootControllerContext.get().getRequest();
+            String url = request.getRequestURI();
+            String contextPath = JFinal.me().getContextPath();
+
+            if (pageNumber == 1 && firstGotoIndex) {
+                return contextPath + "/";
+            }
+
+            // 如果当前页面是首页的话
+            // 需要改变url的值，因为 上一页或下一页是通过当前的url解析出来的
+            if (url.equals(contextPath + "/")) {
+                url = contextPath + "/product/category/index"
+                        + JPressOptions.getAppUrlSuffix();
+            }
+            return DirectveKit.replacePageNumber(url, pageNumber);
+        }
+
+        @Override
+        protected Page<?> getPage(Env env, Scope scope, Writer writer) {
+            return (Page<?>) scope.get("productPage");
+        }
+
+    }
+}

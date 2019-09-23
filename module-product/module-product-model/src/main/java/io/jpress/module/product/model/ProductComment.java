adaf5cc3331c620f965282dc6d8fc2c1a75a4934
retire <wyf372310383@163.com>
2019-09-23 23:20:22 +0800

    jpress3

--- module-product/module-product-model/src/main/java/io/jpress/module/product/model/ProductComment.java
+++ module-product/module-product-model/src/main/java/io/jpress/module/product/model/ProductComment.java
@@ -11,5 +11,8 @@ public class ProductComment extends BaseProductComment<ProductComment> {
 
     private static final long serialVersionUID = 1L;
 
-	
+    public static final String STATUS_NORMAL = "normal"; //正常
+    public static final String STATUS_UNAUDITED = "unaudited"; //待审核
+    public static final String STATUS_TRASH = "trash"; //垃圾箱
+
 }

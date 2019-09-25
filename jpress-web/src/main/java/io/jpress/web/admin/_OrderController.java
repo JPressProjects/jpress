c70d287cadeea5f125149443dc86d6d622c4a23e
fuhai999@gmail.com <fuhai999@gmail.com>
2019-09-25 16:50:40 +0800

    jpress3

--- jpress-web/src/main/java/io/jpress/web/admin/_OrderController.java
+++ jpress-web/src/main/java/io/jpress/web/admin/_OrderController.java
@@ -54,7 +54,7 @@ public class _OrderController extends AdminControllerBase {
     }
 
 
-    @AdminMenu(text = "设置", groupId = JPressConsts.SYSTEM_MENU_ORDER, order = 3)
+    @AdminMenu(text = "收款配置", groupId = JPressConsts.SYSTEM_MENU_ORDER, order = 3)
     public void setting() {
        render("order/setting.html");
     }

8d4467f2122133f9b906eb27cc9e79e8fe8b8223
retire <wyf372310383@163.com>
2019-10-29 13:15:36 +0800

    jpress3

--- jpress-service-provider/src/main/java/io/jpress/service/provider/UserOrderServiceProvider.java
+++ jpress-service-provider/src/main/java/io/jpress/service/provider/UserOrderServiceProvider.java
@@ -72,7 +72,7 @@ public class UserOrderServiceProvider extends JbootServiceBase<UserOrder> implem
 
     @Override
     public int queryMonthUserCount() {
-        String sql = "select count(*) from (select id from user_order where created > ? ) c";
+        String sql = "select count(*) from (select buyer_id from user_order where created > ? group by buyer_id) c";
         return Db.queryInt(sql, DateUtils.truncate(new Date(), Calendar.MONTH));
     }
 }

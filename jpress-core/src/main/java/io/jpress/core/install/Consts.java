39b8a6c689ee4376ae191c9c2c0ab6e8880208ba
fuhai999@gmail.com <fuhai999@gmail.com>
2019-11-21 19:54:37 +0800

    v3.0.0-beta.2

--- jpress-core/src/main/java/io/jpress/core/install/Consts.java
+++ jpress-core/src/main/java/io/jpress/core/install/Consts.java
@@ -32,7 +32,7 @@ class Consts {
             , "attachment", "coupon", "coupon_code", "coupon_product", "coupon_used_record", "member", "member_dist_amount"
             , "member_group", "member_joined_record", "member_price", "menu", "option", "payment_record", "permission", "product"
             , "product_category", "product_category_mapping", "product_comment", "product_image", "role", "role_permission_mapping"
-            , "single_page"
+            , "single_page", "single_page_comment"
             , "user", "user_address", "user_amount", "user_amount_payout", "user_amount_statement", "user_cart", "user_favorite", "user_openid"
             , "user_order", "user_order_delivery", "user_order_invoice", "user_order_item", "user_role_mapping", "utm", "wechat_menu", "wechat_reply");
 }

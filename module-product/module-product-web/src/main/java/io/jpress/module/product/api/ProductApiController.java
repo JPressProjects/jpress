da12363945251ad6dd689c2c2b8c68668aaa26a0
fuhai999@gmail.com <fuhai999@gmail.com>
2019-11-30 16:32:42 +0800

    v3.0.0-rc.2

---
+++ module-product/module-product-web/src/main/java/io/jpress/module/product/api/ProductApiController.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2016-2019, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jpress.module.product.api;
+
+import io.jpress.web.base.ApiControllerBase;
+
+/**
+ * @author michael yang (fuhai999@gmail.com)
+ * @Date: 2019/11/30
+ */
+public class ProductApiController extends ApiControllerBase {
+
+    /**
+     * 获取商品详情
+     */
+    public void index(){
+
+    }
+
+    /**
+     * 获取文章列表
+     */
+    public void list(){
+
+    }
+}

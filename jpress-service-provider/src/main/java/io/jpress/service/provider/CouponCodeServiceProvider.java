161ae6be7719aa8261d29ffd19492bd34abb8c30
fuhai999@gmail.com <fuhai999@gmail.com>
2019-12-12 20:48:44 +0800

    v3.0.0-rc.3

--- jpress-service-provider/src/main/java/io/jpress/service/provider/CouponCodeServiceProvider.java
+++ jpress-service-provider/src/main/java/io/jpress/service/provider/CouponCodeServiceProvider.java
@@ -8,13 +8,16 @@ import io.jboot.db.model.Columns;
 import io.jboot.service.JbootServiceBase;
 import io.jpress.model.Coupon;
 import io.jpress.model.CouponCode;
+import io.jpress.model.Member;
 import io.jpress.service.CouponCodeService;
 import io.jpress.service.CouponService;
+import io.jpress.service.MemberService;
 import io.jpress.service.UserService;
 import org.apache.commons.lang.time.DateUtils;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @Bean
 public class CouponCodeServiceProvider extends JbootServiceBase<CouponCode> implements CouponCodeService {
@@ -25,6 +28,9 @@ public class CouponCodeServiceProvider extends JbootServiceBase<CouponCode> impl
 
     @Inject
     private UserService userService;
+
+    @Inject
+    private MemberService memberService;
 
     @Override
     public Page<CouponCode> paginateByCouponId(int page, int pageSize, Long couponId) {
@@ -37,7 +43,7 @@ public class CouponCodeServiceProvider extends JbootServiceBase<CouponCode> impl
     }
 
     @Override
-    public boolean valid(CouponCode couponCode, BigDecimal orderTotalAmount,long usedUserId) {
+    public boolean valid(CouponCode couponCode, BigDecimal orderTotalAmount, long usedUserId) {
 
         // 该优惠码被标识为：不可用
         if (couponCode == null || !couponCode.isNormal()) {
@@ -51,9 +57,30 @@ public class CouponCodeServiceProvider extends JbootServiceBase<CouponCode> impl
         }
 
         //是否是只有优惠券拥有者可用
-        Boolean withOwner =coupon.getWithOwner();
-        if (withOwner != null && withOwner && !couponCode.getUserId().equals(usedUserId)){
+        Boolean withOwner = coupon.getWithOwner();
+        if (withOwner != null && withOwner && !couponCode.getUserId().equals(usedUserId)) {
             return false;
+        }
+
+        //是不是会员可用
+        Boolean withMember = coupon.getWithMember();
+        if (withMember != null && withMember) {
+            List<Member> members = memberService.findListByUserId(usedUserId);
+            if (members == null || members.isEmpty()) {
+                return false;
+            }
+
+            boolean valid = false;
+            for (Member member : members) {
+                if (member.isNormal()) {
+                    valid = true;
+                    break;
+                }
+            }
+
+            if (!valid) {
+                return false;
+            }
         }
 
 

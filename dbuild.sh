670d8437fb4a5d84b84e6f51044b4c8cf0f275d8
fuhai999@gmail.com <fuhai999@gmail.com>
2019-12-27 18:28:42 +0800

    v3.0.1 release (^.^)YYa!!

--- dbuild.sh
+++ dbuild.sh
@@ -22,3 +22,15 @@ docker push fuhai/jpress:${version}
 echo "exec : docker push fuhai/jpress:latest"
 docker tag fuhai/jpress:${version} fuhai/jpress:latest
 docker push fuhai/jpress:latest
+
+
+echo "exec : docker push jpressio/jpress:"${version}
+docker tag fuhai/jpress:latest jpressio/jpress:${version}
+docker push jpressio/jpress:${version}
+
+
+echo "exec : docker push jpressio/jpress:latest"
+docker tag jpressio/jpress:${version} jpressio/jpress:latest
+docker push jpressio/jpress:latest
+
+

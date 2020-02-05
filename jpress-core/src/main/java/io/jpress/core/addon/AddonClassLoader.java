ebbdbdcefba7d69a59b5ce3517147e0538056e4e
yangfuhai <fuhai999@gmail.com>
2020-02-05 11:01:15 +0800

    add NotGlobalInterceptor annotation for addon

--- jpress-core/src/main/java/io/jpress/core/addon/AddonClassLoader.java
+++ jpress-core/src/main/java/io/jpress/core/addon/AddonClassLoader.java
@@ -29,6 +29,7 @@ import io.jboot.components.event.JbootEventListener;
 import io.jboot.components.mq.JbootmqMessageListener;
 import io.jboot.db.model.JbootModel;
 import io.jboot.utils.ArrayUtil;
+import io.jpress.core.addon.annotation.NotGlobalInterceptor;
 import io.jpress.core.wechat.WechatAddon;
 
 import java.io.IOException;
@@ -91,7 +92,9 @@ public class AddonClassLoader extends URLClassLoader {
                 }
                 // interceptors
                 else if (Interceptor.class.isAssignableFrom(loadedClass)) {
-                    addonInfo.addInterceptor(loadedClass);
+                    if (loadedClass.getAnnotation(NotGlobalInterceptor.class) == null) {
+                        addonInfo.addInterceptor(loadedClass);
+                    }
                 }
                 // handlers
                 else if (Handler.class.isAssignableFrom(loadedClass)) {

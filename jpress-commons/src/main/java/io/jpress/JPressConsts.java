69266f63e6c761ac0567d9fa3a3aefb29433d199
yangfuhai <fuhai999@gmail.com>
2021-07-23 09:45:18 +0800

    JPress v4.1.0 release (^.^)YYa!!

--- jpress-commons/src/main/java/io/jpress/JPressConsts.java
+++ jpress-commons/src/main/java/io/jpress/JPressConsts.java
@@ -23,12 +23,12 @@ package io.jpress;
  */
 public class JPressConsts {
 
-    public static final String VERSION = "v4.0.9";
+    public static final String VERSION = "v4.1.0";
 
     //v2.x not define
     //v3.x  VERSION_CODE < 40
     //v4.x  VERSION_CODE >= 40
-    public static final String VERSION_CODE = "49";
+    public static final String VERSION_CODE = "50";
 
     /**
      * 后台系统菜单的 ID

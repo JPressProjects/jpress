f223d10506b42a0a990a99f3686b6725c1fc4f77
yangfuhai <fuhai999@gmail.com>
2021-09-24 16:10:16 +0800

    编辑器添加对其方式，图片和表格添加更多的属性操作。修复某些内容不能粘贴到编辑器的问题。

--- jpress-commons/src/main/java/io/jpress/JPressConsts.java
+++ jpress-commons/src/main/java/io/jpress/JPressConsts.java
@@ -23,12 +23,12 @@ package io.jpress;
  */
 public class JPressConsts {
 
-    public static final String VERSION = "v4.1.2";
+    public static final String VERSION = "v4.1.3";
 
     //v2.x not define
     //v3.x  VERSION_CODE < 40
     //v4.x  VERSION_CODE >= 40
-    public static final String VERSION_CODE = "52";
+    public static final String VERSION_CODE = "53";
 
     /**
      * 后台系统菜单的 ID

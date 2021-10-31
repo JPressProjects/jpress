9e30ed42fcaef5e5b03e980f34429a2f87a5ce11
yangfuhai <fuhai999@gmail.com>
2021-10-31 17:19:38 +0800

    优化...

--- jpress-service/src/main/java/io/jpress/service/UtmService.java
+++ jpress-service/src/main/java/io/jpress/service/UtmService.java
@@ -18,71 +18,7 @@ package io.jpress.service;
 import com.jfinal.plugin.activerecord.Page;
 import io.jpress.model.Utm;
 
-import java.util.List;
-
 public interface UtmService {
-
-    /**
-     * 根据 主键 查找 Model
-     *
-     * @param id
-     * @return
-     */
-    Utm findById(Object id);
-
-
-    /**
-     * 查询所有的数据
-     *
-     * @return 所有的 Utm
-     */
-    List<Utm> findAll();
-
-
-    /**
-     * 根据主键删除 Model
-     *
-     * @param id
-     * @return success
-     */
-    boolean deleteById(Object id);
-
-
-    /**
-     * 删除 Model
-     *
-     * @param model
-     * @return
-     */
-    boolean delete(Utm model);
-
-
-    /**
-     * 新增 Model 数据
-     *
-     * @param model
-     * @return
-     */
-    Object save(Utm model);
-
-
-    /**
-     * 新增或者更新 Model 数据（主键值为 null 就新增，不为 null 则更新）
-     *
-     * @param model
-     * @return 新增或更新成功后，返回该 Model 的主键值
-     */
-    Object saveOrUpdate(Utm model);
-
-
-    /**
-     * 更新此 Model 的数据，务必要保证此 Model 的主键不能为 null
-     *
-     * @param model
-     * @return
-     */
-    boolean update(Utm model);
-
 
     Page<Utm> paginate(int page, int pagesize);
 

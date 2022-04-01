82ee1ea7a1601d4dd9ed99e731d9eca78f4319da
yangfuhai <fuhai999@gmail.com>
2022-04-01 11:43:44 +0800

    JPress v4.2.1 release (^.^)YYa!!

--- jpress-commons/src/main/java/io/jpress/JPressConsts.java
+++ jpress-commons/src/main/java/io/jpress/JPressConsts.java
@@ -23,7 +23,7 @@ package io.jpress;
  */
 public class JPressConsts {
 
-    public static final String VERSION = "v4.2.0";
+    public static final String VERSION = "v4.2.1";
 
     //v2.x not define
     //v3.x  VERSION_CODE < 40

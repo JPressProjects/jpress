fe0878e8f2e893faab5397206755144aafc96205
yi_pengfei <2586283122@qq.com>
2022-06-22 11:09:00 +0800

    视频 直播功能

--- jpress-web/src/main/java/io/jpress/web/admin/_AttachmentVideoController.java
+++ jpress-web/src/main/java/io/jpress/web/admin/_AttachmentVideoController.java
@@ -5,8 +5,10 @@ import com.jfinal.kit.Ret;
 import com.jfinal.log.Log;
 import com.jfinal.plugin.activerecord.Page;
 import io.jboot.db.model.Columns;
+import io.jboot.utils.StrUtil;
 import io.jboot.web.controller.annotation.RequestMapping;
 import io.jpress.JPressConsts;
+import io.jpress.commons.aliyun.AliyunLiveUtil;
 import io.jpress.commons.aliyun.AliyunVideoUtil;
 import io.jpress.commons.aliyun.CloudVideoInfo;
 import io.jpress.core.menu.annotation.AdminMenu;
@@ -60,11 +62,16 @@ public class _AttachmentVideoController extends AdminControllerBase {
     public void edit(){
         String id = getPara();
         AttachmentVideo video = attachmentVideoService.findById(id);
-        if(video.getVodVid() != null){
-            String playauth = AliyunVideoUtil.getPlayAuth(video.getVodVid());
-            setAttr("cloudPlayAuth", playauth);
-            setAttr("cloudVid", video.getVodVid());
-        }
+        String playauth = AliyunVideoUtil.getPlayAuth(video.getVodVid());
+        //点播视频
+        setAttr("cloudPlayAuth", playauth);
+        setAttr("cloudVid", video.getVodVid());
+
+        //直播, m3u8 是延迟最高（延迟在 40s 左右）的，但是浏览器的兼容性是最好的
+        setAttr("livePlayUrl", AliyunLiveUtil.createPlayUrlForM3U8(video.getLiveStream()));
+        //直播回放
+        setAttr("liveCloudPlayAuth", playauth);
+        setAttr("liveCloudVid", video.getVodVid());
 
         setAttr("video",video);
 
@@ -171,6 +178,25 @@ public class _AttachmentVideoController extends AdminControllerBase {
      */
     public Ret doGetPlayAuth(String videoId) {
         return Ret.ok().set("playAuth", AliyunVideoUtil.getPlayAuth(videoId));
+    }
+
+
+    /**
+     * 创建直播流
+     *
+     * @return
+     */
+    public Ret doGetLiveInfo() {
+        String streamName = StrUtil.uuid();
+
+        String appName = AliyunLiveUtil.getAppName();
+        String playDomain = AliyunLiveUtil.getPlayDomain();
+
+        String pushUrl = AliyunLiveUtil.createPushUrl(streamName);
+        String playUrl = AliyunLiveUtil.createPlayUrlForM3U8(streamName);
+
+        return Ret.ok().set("pushUrl", pushUrl).set("playUrl", playUrl).set("liveApp", appName)
+                        .set("domainName", playDomain).set("streamName", streamName);
     }
 
 

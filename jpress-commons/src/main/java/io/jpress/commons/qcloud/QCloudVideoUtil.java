3cb57eef9854c00f0b5cc4e975fceb039a3683b3
yi_pengfei <2586283122@qq.com>
2022-06-27 15:48:57 +0800

    修改

--- jpress-commons/src/main/java/io/jpress/commons/qcloud/QCloudVideoUtil.java
+++ jpress-commons/src/main/java/io/jpress/commons/qcloud/QCloudVideoUtil.java
@@ -1,26 +1,7 @@
 package io.jpress.commons.qcloud;
 
-import com.tencentcloudapi.common.Credential;
-import com.tencentcloudapi.common.exception.TencentCloudSDKException;
-import com.tencentcloudapi.cvm.v20170312.CvmClient;
-import com.tencentcloudapi.cvm.v20170312.models.DescribeInstancesRequest;
-import com.tencentcloudapi.cvm.v20170312.models.DescribeInstancesResponse;
-
 public class QCloudVideoUtil {
 
 
-    public void getAppId() {
-        try {
-            Credential cred = new Credential("secretId", "secretKey");
-            CvmClient client = new CvmClient(cred, "ap-shanghai");
 
-            DescribeInstancesRequest req = new DescribeInstancesRequest();
-            DescribeInstancesResponse resp = client.DescribeInstances(req);
-
-            System.out.println(DescribeInstancesResponse.toJsonString(resp));
-        } catch (TencentCloudSDKException e) {
-            System.out.println(e.toString());
-        }
-
-    }
 }

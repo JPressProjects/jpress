e477620092ceed2cd15c14778323deaf36fe5d38
yi_pengfei <2586283122@qq.com>
2022-06-27 15:46:35 +0800

    新增

--- jpress-commons/src/main/java/io/jpress/commons/qcloud/QCloudVideoUtil.java
+++ jpress-commons/src/main/java/io/jpress/commons/qcloud/QCloudVideoUtil.java
@@ -9,5 +9,18 @@ import com.tencentcloudapi.cvm.v20170312.models.DescribeInstancesResponse;
 public class QCloudVideoUtil {
 
 
+    public void getAppId() {
+        try {
+            Credential cred = new Credential("secretId", "secretKey");
+            CvmClient client = new CvmClient(cred, "ap-shanghai");
 
+            DescribeInstancesRequest req = new DescribeInstancesRequest();
+            DescribeInstancesResponse resp = client.DescribeInstances(req);
+
+            System.out.println(DescribeInstancesResponse.toJsonString(resp));
+        } catch (TencentCloudSDKException e) {
+            System.out.println(e.toString());
+        }
+
+    }
 }

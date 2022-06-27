e5918eed68b7872336422a113be15358eb91ba5f
yi_pengfei <2586283122@qq.com>
2022-06-27 15:44:42 +0800

    新增

---
+++ jpress-commons/src/main/java/io/jpress/commons/qcloud/QCloudVideoUtil.java
@@ -0,0 +1,13 @@
+package io.jpress.commons.qcloud;
+
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+import com.tencentcloudapi.cvm.v20170312.CvmClient;
+import com.tencentcloudapi.cvm.v20170312.models.DescribeInstancesRequest;
+import com.tencentcloudapi.cvm.v20170312.models.DescribeInstancesResponse;
+
+public class QCloudVideoUtil {
+
+
+
+}

8e283acc79a9e14fca69b17dbc312d2601929fbe
开源海哥 <fuhai999@gmail.com>
2022-06-30 17:57:31 +0800

    完善模板的模块 "拖拽" 功能

--- jpress-core/src/main/java/io/jpress/core/template/BlockManager.java
+++ jpress-core/src/main/java/io/jpress/core/template/BlockManager.java
@@ -17,17 +17,20 @@ package io.jpress.core.template;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.jfinal.core.JFinal;
 import com.jfinal.render.RenderManager;
 import com.jfinal.template.Engine;
 import io.jboot.utils.StrUtil;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.util.*;
 
 public class BlockManager {
 
+    private static String contextPath = JFinal.me().getContextPath();
 
     //系统自带的 html 模块
     private List<BlockHtml> systemBlockHtmls = new ArrayList<>();
@@ -112,11 +115,69 @@ public class BlockManager {
             html = "<div></div>";
         }
         Document fragment = Jsoup.parseBodyFragment(html);
+        buildNormalHtml(fragment);
         Element body = fragment.body();
         Element htmlElement = body.child(0);
         htmlElement.attr("id", id);
         htmlElement.addClass("bsFormItem");
         return htmlElement.toString();
+    }
+
+
+
+
+
+    public void buildNormalHtml(Document doc)  {
+
+        Elements jsElements = doc.select("script");
+        replace(jsElements, "src");
+
+        Elements imgElements = doc.select("img");
+        replace(imgElements, "src");
+
+        Elements sourceElements = doc.select("source");
+        replace(sourceElements, "src");
+
+        Elements linkElements = doc.select("link");
+        replace(linkElements, "href");
+    }
+
+    private void replace(Elements elements, String attrName) {
+         Template currentTemplate = TemplateManager.me().getCurrentTemplate();
+
+        Iterator<Element> iterator = elements.iterator();
+        while (iterator.hasNext()) {
+
+            Element element = iterator.next();
+            String url = element.attr(attrName);
+
+            if (StrUtil.isBlank(url)
+                    || url.startsWith("//")
+                    || url.toLowerCase().startsWith("http")
+                    || (attrName.equals("src") && url.startsWith("data:image/"))
+                    || element.hasAttr("cdn-exclude")) {
+                continue;
+            }
+
+            // 以 / 开头的，需要添加 contextPath
+            if (url.startsWith("/")) {
+                if (contextPath.length() > 0 && url.startsWith(contextPath + "/") == false) {
+                    url = contextPath + url;
+                }
+            }
+
+            // 以 ./ 开头的文件，需要添加模板路径
+            else if (url.startsWith("./")) {
+                url = contextPath + currentTemplate.getRelativePath() + url.substring(1);
+            }
+
+            // 直接是文件目录名开头
+            else {
+                url = contextPath + currentTemplate.getRelativePath() + "/" + url;
+            }
+
+            element.attr(attrName, url);
+        }
     }
 
 

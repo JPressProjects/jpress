c996d0a35c68ffe8b21abd8efa8f3514dda6e8da
开源海哥 <fuhai999@gmail.com>
2022-07-12 18:26:35 +0800

    update ...

---
+++ module-form/module-form-web/src/main/java/io/jpress/module/form/controller/front/FormController.java
@@ -0,0 +1,18 @@
+package io.jpress.module.form.controller.front;
+
+import io.jboot.web.controller.JbootController;
+import io.jboot.web.controller.annotation.RequestMapping;
+
+@RequestMapping("/form")
+public class FormController extends JbootController {
+
+
+
+
+    /**
+     * 提交数据到 form
+     */
+    public void postData(){
+
+    }
+}

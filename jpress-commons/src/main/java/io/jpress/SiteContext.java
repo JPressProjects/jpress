906fab83dfb9f66521a6423b0f8a9de36b4579b3
开源海哥 <fuhai999@gmail.com>
2022-08-22 17:02:13 +0800

    JPress v5.0.0 release (^.^)YYa!!

--- jpress-commons/src/main/java/io/jpress/SiteContext.java
+++ jpress-commons/src/main/java/io/jpress/SiteContext.java
@@ -1,7 +1,10 @@
 package io.jpress;
 
 import com.jfinal.core.Controller;
+import com.jfinal.kit.Func;
 import com.jfinal.plugin.activerecord.Model;
+import io.jboot.components.cache.JbootCache;
+import io.jboot.components.cache.JbootCacheManager;
 import io.jboot.utils.StrUtil;
 import io.jboot.web.controller.JbootControllerContext;
 
@@ -9,32 +12,68 @@ public class SiteContext {
 
     private static ThreadLocal<Long> TL = new ThreadLocal<>();
 
-    public static void setSiteId(Long siteId){
+    public static void setSiteId(Long siteId) {
         TL.set(siteId);
     }
 
-    public static Long getSiteId(){
+    public static Long getSiteId() {
         Long siteId = TL.get();
         return siteId == null ? 0L : siteId;
     }
 
-    public static void removeSiteId(){
+    public static void removeSiteId() {
         TL.remove();
     }
 
-    public static String getSitePath(){
+    public static String getSitePath() {
         Controller controller = JbootControllerContext.get();
-        if (controller == null){
+        if (controller == null) {
             return "";
         }
 
         Model site = controller.getAttr("SITE");
-        if (site == null){
+        if (site == null) {
             return "";
         }
 
         String path = site.getStr("bind_path");
         return StrUtil.isNotBlank(path) ? path : "";
     }
+
+
+    public static void execInMainSite(Func.F00 callable) {
+        Long currentSiteId = getSiteId();
+        if (currentSiteId == 0) {
+            callable.call();
+        } else {
+            JbootCache cache = JbootCacheManager.me().getCache();
+            try {
+                setSiteId(0L);
+                cache.setCurrentCacheNamePrefix("site0:");
+                callable.call();
+            } finally {
+                setSiteId(currentSiteId);
+                cache.setCurrentCacheNamePrefix("site" + currentSiteId + ":");
+            }
+        }
+    }
+
+    public static <T> T execInMainSite(Func.F01<T> callable) {
+        Long currentSiteId = getSiteId();
+        if (currentSiteId == 0) {
+            return callable.call();
+        } else {
+            JbootCache cache = JbootCacheManager.me().getCache();
+            try {
+                setSiteId(0L);
+                cache.setCurrentCacheNamePrefix("site0:");
+                return callable.call();
+            } finally {
+                setSiteId(currentSiteId);
+                cache.setCurrentCacheNamePrefix("site" + currentSiteId + ":");
+            }
+        }
+    }
+
 
 }
--- jpress-commons/src/main/java/io/jpress/commons/utils/SessionUtils.java
+++ jpress-commons/src/main/java/io/jpress/commons/utils/SessionUtils.java
@@ -3,6 +3,7 @@ package io.jpress.commons.utils;
 import io.jboot.Jboot;
 import io.jboot.utils.CacheUtil;
 import io.jboot.utils.StrUtil;
+import io.jpress.SiteContext;
 
 /**
  * @author michael yang (fuhai999@gmail.com)
@@ -12,19 +13,19 @@ public class SessionUtils {
     private static final String SESSION_NAME = "userSessions";
 
     public static void record(Object userId) {
-        CacheUtil.put(SESSION_NAME, String.valueOf(userId), StrUtil.uuid());
+        SiteContext.execInMainSite(() -> CacheUtil.put(SESSION_NAME, String.valueOf(userId), StrUtil.uuid()));
     }
 
     public static void forget(Object userId) {
-        CacheUtil.remove(SESSION_NAME, String.valueOf(userId));
+        SiteContext.execInMainSite(() -> CacheUtil.remove(SESSION_NAME, String.valueOf(userId)));
     }
 
     public static String getSessionId(Object userId) {
-        return CacheUtil.get(SESSION_NAME, String.valueOf(userId));
+        return SiteContext.execInMainSite(() -> CacheUtil.get(SESSION_NAME, String.valueOf(userId)));
     }
 
     public static boolean isLoginedOk(Object userId) {
         // 开发模式下，不通过 Session 控制，否则因为使用内存缓存，每次重启都需要重新登录，影响开发体验
-        return Jboot.isDevMode() || StrUtil.isNotBlank(getSessionId(userId));
+        return SiteContext.execInMainSite(() -> Jboot.isDevMode() || StrUtil.isNotBlank(getSessionId(userId)));
     }
 }

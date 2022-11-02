fa071ebb53fa855e7ec7c0b1253b6777e9bfb7fe
LENOVO <gongxr@jxebupt.com>
2022-11-02 11:05:34 +0800

    fix:修复选择文件，文件名称不回显的问题

--- jpress-web/src/main/webapp/static/front/jpressfront.js
+++ jpress-web/src/main/webapp/static/front/jpressfront.js
@@ -956,6 +956,13 @@ function jobFileChoose() {
 
     })
 }
+// 上传文件回显
+function initBsFormFileComponent(){
+    $("body .jpress-custom-file").on("change",'.jpress-file-input',function () {
+        var fileName = $(this).val();
+        $(this).next('.jpress-file-label').html(fileName)
+    })
+}
 
 //图片组件
 function initBsFormImageComponent() {
@@ -1079,6 +1086,8 @@ $(document).ready(function () {
     /*job apply 页面 文件选择*/
     jobFileChoose();
 
+    // 文件回显
+    initBsFormFileComponent();
     /*图片组件*/
     initBsFormImageComponent()
 

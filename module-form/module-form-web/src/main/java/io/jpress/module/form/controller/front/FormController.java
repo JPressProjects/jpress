0b6f4e585b47077b67d7d62b66e406d4ed6b6017
开源海哥 <fuhai999@gmail.com>
2022-11-13 10:27:21 +0800

    fixed form without upload file error

--- module-form/module-form-web/src/main/java/io/jpress/module/form/controller/front/FormController.java
+++ module-form/module-form-web/src/main/java/io/jpress/module/form/controller/front/FormController.java
@@ -4,6 +4,7 @@ import com.anji.captcha.model.common.ResponseModel;
 import com.anji.captcha.model.vo.CaptchaVO;
 import com.anji.captcha.service.CaptchaService;
 import com.jfinal.aop.Inject;
+import com.jfinal.kit.LogKit;
 import com.jfinal.kit.Ret;
 import com.jfinal.plugin.activerecord.Record;
 import com.jfinal.upload.UploadFile;
@@ -72,7 +73,7 @@ public class FormController extends TemplateControllerBase {
 
 
         List<UploadFile> files = null;
-        if (RequestUtil.isMultipartRequest(getRequest())){
+        if (RequestUtil.isMultipartRequest(getRequest())) {
             files = getFiles();
         }
 
@@ -80,9 +81,7 @@ public class FormController extends TemplateControllerBase {
 
         //进行前端滑块 参数验证
         if (captchaVO == null || captchaVO.getCaptchaVerification() == null) {
-            if (files != null) {
-                files.forEach(FileUtil::delete);
-            }
+            deleteFiles(files);
             renderFailJson("验证失败");
             return;
         }
@@ -90,15 +89,14 @@ public class FormController extends TemplateControllerBase {
 
         ResponseModel validResult = captchaService.verification(captchaVO);
         if (validResult == null || !validResult.isSuccess()) {
-            if (files != null) {
-                files.forEach(FileUtil::delete);
-            }
+            deleteFiles(files);
             renderFailJson("验证失败");
             return;
         }
 
         FormInfo formInfo = formInfoService.findByUUID(uuid);
         if (formInfo == null || !formInfo.isPublished()) {
+            deleteFiles(files);
             renderError(404);
             return;
         }
@@ -118,10 +116,15 @@ public class FormController extends TemplateControllerBase {
         try {
             // parseRequestToRecord 可能会出现数据转换异常，需要告知前端
             Record record = formInfo.parseRequestToRecord(getRequest());
-            Map<String, String> uploadFilePaths = getFilePaths(uploadFileNames);
-            for (FieldInfo fieldInfo : fieldInfos) {
-                if (fieldInfo.isSupportUpload()) {
-                    record.set(fieldInfo.getFieldName(),uploadFilePaths.get(fieldInfo.getParaName()));
+
+
+            //有上传文件
+            if (uploadFileNames.size() > 0) {
+                Map<String, String> uploadFilePaths = getFilePaths(uploadFileNames);
+                for (FieldInfo fieldInfo : fieldInfos) {
+                    if (fieldInfo.isSupportUpload()) {
+                        record.set(fieldInfo.getFieldName(), uploadFilePaths.get(fieldInfo.getParaName()));
+                    }
                 }
             }
 
@@ -135,28 +138,35 @@ public class FormController extends TemplateControllerBase {
 
         } catch (IllegalArgumentException iae) {
             renderJson(Ret.fail().set("message", iae.getMessage()));
-            if (files != null) {
-                files.forEach(FileUtil::delete);
-            }
             return;
         } catch (Exception e) {
             e.printStackTrace();
             renderJson(Ret.fail().set("message", "数据提交失败，请联系管理员"));
-            if (files != null) {
-                files.forEach(FileUtil::delete);
-            }
             return;
+        }finally {
+            if (files != null) {
+                for (UploadFile file : files) {
+                    if (file.getFile().exists() && !file.getFile().delete()){
+                        LogKit.error("Can not delete file: {}",file.getFile().getAbsolutePath());
+                    }
+                }
+            }
         }
 
         renderOkJson();
     }
 
-    private Map<String, String> getFilePaths(Set<String> names) {
-        if (names == null || names.isEmpty()) {
-            throw new IllegalArgumentException("names can not be null or empty.");
+    private void deleteFiles(List<UploadFile> files) {
+        if (files != null) {
+            files.forEach(FileUtil::delete);
         }
+    }
+
+
+    private Map<String, String> getFilePaths(Set<String> names) {
 
         Map<String, String> filesMap = new HashMap<>();
+
         List<UploadFile> uploadFiles = getFiles();
         if (uploadFiles == null || uploadFiles.isEmpty()) {
             return filesMap;
@@ -169,19 +179,19 @@ public class FormController extends TemplateControllerBase {
                 continue;
             }
 
-            parameterName = matchedParameterName(parameterName.trim(),names);
+            parameterName = matchedParameterName(parameterName.trim(), names);
 
             if (StrUtil.isNotBlank(parameterName)) {
                 String path = AttachmentUtils.moveFile(uploadFile);
                 AliyunOssUtils.upload(path, AttachmentUtils.file(path));
 
-                String orignalPath = filesMap.get(parameterName);
-                if (orignalPath == null){
-                    orignalPath = path;
-                }else {
-                    orignalPath = orignalPath+";"+path;
+                String exitsPath = filesMap.get(parameterName);
+                if (exitsPath == null) {
+                    exitsPath = path;
+                } else {
+                    exitsPath = exitsPath + ";" + path;
                 }
-                filesMap.put(parameterName, orignalPath);
+                filesMap.put(parameterName, exitsPath);
             } else {
                 FileUtil.delete(uploadFile);
             }
@@ -194,19 +204,20 @@ public class FormController extends TemplateControllerBase {
     /**
      * JFinal 有一个 bug，相同 name 属性的文件选择框，第二个 name 会添加索引 _0,_1....
      * 这个方法用来匹配到真正的 name
+     *
      * @param paraName
      * @param names
      * @return
      */
-    private String matchedParameterName(String paraName,Set<String> names){
-        if (names.contains(paraName)){
+    private String matchedParameterName(String paraName, Set<String> names) {
+        if (names.contains(paraName)) {
             return paraName;
         }
 
         int indexOf = paraName.lastIndexOf("_");
-        if (indexOf > 0 && StrUtil.isNumeric(paraName.substring(indexOf + 1))){
-            paraName = paraName.substring(0,indexOf);
-            if (names.contains(paraName)){
+        if (indexOf > 0 && StrUtil.isNumeric(paraName.substring(indexOf + 1))) {
+            paraName = paraName.substring(0, indexOf);
+            if (names.contains(paraName)) {
                 return paraName;
             }
         }

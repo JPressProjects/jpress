26063ffdccfd4d02ae52717b47c202ed3ad8d21c
开源海哥 <fuhai999@gmail.com>
2022-12-31 22:39:25 +0800

    优化 TemplateBlockOptionServiceProvider.java

--- jpress-service-provider/src/main/java/io/jpress/service/provider/TemplateBlockOptionServiceProvider.java
+++ jpress-service-provider/src/main/java/io/jpress/service/provider/TemplateBlockOptionServiceProvider.java
@@ -1,6 +1,10 @@
 package io.jpress.service.provider;
 
+import com.jfinal.plugin.activerecord.Model;
 import io.jboot.aop.annotation.Bean;
+import io.jboot.components.cache.annotation.CacheEvict;
+import io.jboot.components.cache.annotation.Cacheable;
+import io.jboot.components.cache.annotation.CachesEvict;
 import io.jpress.commons.service.JPressServiceBase;
 import io.jpress.model.TemplateBlockOption;
 import io.jpress.service.TemplateBlockOptionService;
@@ -9,7 +13,16 @@ import io.jpress.service.TemplateBlockOptionService;
 public class TemplateBlockOptionServiceProvider extends JPressServiceBase<TemplateBlockOption> implements TemplateBlockOptionService {
 
     @Override
+    @Cacheable(name = "templateBlockOption", nullCacheEnable = true)
     public TemplateBlockOption findById(Object templateId, Long siteId) {
         return DAO.findByIds(templateId, siteId);
+    }
+
+    @Override
+    @CachesEvict({
+            @CacheEvict(name = "templateBlockOption", key = "*")
+    })
+    public void shouldUpdateCache(int action, Model model, Object id) {
+
     }
 }
